e19794f8815e3eb727936ea97381e8bc3b3e70dc
Ethan Roseman <ethteck@gmail.com>
2021-07-07 01:54:41 +0900

    Clean up CompilerWrapper

--- backend/coreapp/compilerwrapper.py
+++ backend/coreapp/compilerwrapper.py
@@ -21,6 +21,52 @@ class CompilerWrapper:
         CompilerWrapper.base_path = Path(os.environ["COMPILER_BASE_PATH"]).resolve()
 
     @staticmethod
+    def run_compiler(compile_cmd: str, input_path: Path, output_path: Path, compiler_path: Path, cc_flags: str):
+        compile_command = compile_cmd \
+            .replace("$INPUT", str(input_path)) \
+            .replace("$OUTPUT", str(output_path)) \
+            .replace("$COMPILER_PATH", str(compiler_path)) \
+            .replace("$CC_FLAGS", cc_flags)
+
+        logger.debug(f"Compiling: {compile_command}")
+
+        try:
+            result = subprocess.run(compile_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            
+            if result.returncode != 0:
+                logger.error(result.stderr.decode())
+                return (1, "Non-zero error code from compiler")
+        except Exception as e:
+            logger.error(e)
+            return (2, "Exception while running compiler")
+        
+        if not output_path.exists():
+            logger.error(f"Compiled object does not exist: {str(output_path)}")
+            return (3, "Compiled object does not exist")
+
+        return (0, )
+
+    @staticmethod
+    def run_objdump(object_path: Path):
+        objdump_command = "mips-linux-gnu-objdump -drz -j .text " + str(object_path)
+
+        logger.debug(f"Objdumping: {objdump_command}")
+
+        try:
+            result = subprocess.run(objdump_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+            if result.returncode != 0:
+                logger.error(result.stderr.decode())
+                return (4, "Non-zero error code from objdump")
+
+            output = result.stdout.decode()
+        except Exception as e:
+            logger.error(e)
+            return (5, "Exception while running objdump")
+
+        return (0, output)
+
+    @staticmethod
     def compile_code(compiler_config: CompilerConfiguration, code: str):
         CompilerWrapper.initialize_base_path()
 
@@ -31,60 +77,39 @@ class CompilerWrapper:
         if not compiler_path.exists():
             logger.error(f"Compiler {compiler.shortname} not found")
             return "ERROR: Compiler not found"
-        
-        temp_name = get_random_string(length=8)
-        temp_c_file = CompilerWrapper.base_path / (temp_name + ".txt")
-        temp_o_file = CompilerWrapper.base_path / (temp_name + ".o")
 
-        with open(temp_c_file, "w", newline="\n") as f:
+        temp_name = get_random_string(length=8)
+        code_path = CompilerWrapper.base_path / (temp_name + ".txt")
+        object_path = CompilerWrapper.base_path / (temp_name + ".o")
+
+        with open(code_path, "w", newline="\n") as f:
             f.write(code)
 
-        compile_command = compiler.compile_cmd
-        compile_command = compile_command.replace("$INPUT", str(temp_c_file))
-        compile_command = compile_command.replace("$OUTPUT", str(temp_o_file))
-        compile_command = compile_command.replace("$COMPILER_PATH", str(compiler_path))
-        compile_command = compile_command.replace("$CC_FLAGS", compiler_config.flags)
-        
-        objdump_command = "mips-linux-gnu-objdump -drz -j .text " + str(temp_o_file)
-
         # Run compiler
-        try:
-            logger.debug(f"Compiling: {compile_command}")
-            result = subprocess.run(compile_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            
-            logger.debug(f"test123")
-            if result.returncode != 0:
-                logger.error(result.stderr.decode())
-                return "ERROR: Could not run compiler"
-        except Exception as e:
-            os.remove(temp_c_file)
-            logger.error(e)
-            return "ERROR: Could not run compiler"
-        
-        if not temp_o_file.exists():
-            os.remove(temp_c_file)
-            logger.error(f"Compiled object does not exist: {temp_o_file}")
-            return "ERROR: Compiled object does not exist"
+        compile_status = CompilerWrapper.run_compiler(
+            compiler.compile_cmd,
+            code_path,
+            object_path,
+            compiler_path,
+            compiler_config.flags
+        )
+
+        os.remove(code_path)
+
+        # Compilation failed
+        if compile_status[0] != 0:
+            if object_path.exists():
+                os.remove(object_path)
+            return f"ERROR: {compile_status[1]}"
 
         # Run objdump
-        try:
-            logger.debug(f"Objdumping: {objdump_command}")
-            result = subprocess.run(objdump_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        objdump_status = CompilerWrapper.run_objdump(object_path)
 
-            if result.returncode != 0:
-                logger.error(result.stderr.decode())
-                os.remove(temp_c_file)
-                os.remove(temp_o_file)
-                return "ERROR: Could not run objdump"
+        if object_path.exists():
+            os.remove(object_path)
 
-            output = result.stdout.decode()
-        except Exception as e:
-            os.remove(temp_c_file)
-            os.remove(temp_o_file)
-            logger.error(e)
-            return "ERROR: Could not run objdump"
+        # Objdump failed
+        if objdump_status[0] != 0:
+            return f"ERROR: {objdump_status[1]}"
 
-        os.remove(temp_c_file)
-        os.remove(temp_o_file)
-
-        return output
+        return objdump_status[1]
