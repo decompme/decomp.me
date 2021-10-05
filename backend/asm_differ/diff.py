27be92be382679c479b28897ae47026bc933ac93
Ethan Roseman <ethteck@gmail.com>
2021-10-05 21:43:23 +0900

    git subrepo pull backend/asm_differ
    subrepo:
      subdir:   "backend/asm_differ"
      merged:   "88a68d0"
    upstream:
      origin:   "https://github.com/simonlindholm/asm-differ"
      branch:   "main"
      commit:   "88a68d0"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

--- backend/asm_differ/diff.py
+++ backend/asm_differ/diff.py
@@ -488,7 +488,12 @@ def get_objdump_executable(objdump_executable: Optional[str]) -> str:
     if objdump_executable is not None:
         return objdump_executable
 
-    for objdump_cand in ["mips-linux-gnu-objdump", "mips64-elf-objdump"]:
+    objdump_candidates = [
+        "mips-linux-gnu-objdump",
+        "mips64-elf-objdump",
+        "mips-elf-objdump",
+    ]
+    for objdump_cand in objdump_candidates:
         try:
             subprocess.check_call(
                 [objdump_cand, "--version"],
@@ -502,7 +507,7 @@ def get_objdump_executable(objdump_executable: Optional[str]) -> str:
             pass
 
     return fail(
-        "Missing binutils; please ensure mips-linux-gnu-objdump or mips64-elf-objdump exist, or configure objdump_executable."
+        f"Missing binutils; please ensure {' or '.join(objdump_candidates)} exists, or configure objdump_executable."
     )
 
 
@@ -1636,6 +1641,11 @@ def parse_relocated_line(line: str) -> Tuple[str, str, str]:
 
 
 def process_mips_reloc(row: str, prev: str, arch: ArchSettings) -> str:
+    if "R_MIPS_NONE" in row:
+        # GNU as emits no-op relocations immediately after real ones when
+        # assembling with -mabi=64. Return without trying to parse 'imm' as an
+        # integer.
+        return prev
     before, imm, after = parse_relocated_line(prev)
     repl = row.split()[-1]
     if imm != "0":
@@ -2375,6 +2385,7 @@ def do_diff(lines1: List[Line], lines2: List[Line], config: Config) -> Diff:
             )
         )
 
+    output = output[config.skip_lines :]
     return Diff(lines=output, score=score)
 
 
@@ -2608,10 +2619,9 @@ class Display:
             self.last_diff_output = diff_output
 
         meta, diff_lines = align_diffs(last_diff_output, diff_output, self.config)
-        diff_lines = diff_lines[self.config.skip_lines :]
         output = self.config.formatter.table(meta, diff_lines)
         refresh_key = (
-            [[col.key2 for col in x[1:]] for x in diff_lines],
+            [line.key2 for line in diff_output.lines],
             diff_output.score,
         )
         return (output, refresh_key)
