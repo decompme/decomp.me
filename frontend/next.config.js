0e36ef2229b9c0f8c5e57eeb15bc06fe43898159
alex <16batesa@gmail.com>
2021-10-11 16:20:07 +0100

    add footer

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,5 @@
 const { config } = require("dotenv")
+const { execSync } = require("child_process")
 
 for (const envFile of [".env.local", ".env"]) {
     config({ path: `../${envFile}` })
@@ -6,6 +7,7 @@ for (const envFile of [".env.local", ".env"]) {
 
 process.env.NEXT_PUBLIC_API_BASE = process.env.API_BASE
 process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
+process.env.NEXT_PUBLIC_COMMIT_HASH = execSync("git rev-parse HEAD").toString().trim()
 
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,43 @@
+import Link from "next/link"
+
+import { MarkGithubIcon } from "@primer/octicons-react"
+
+import Discord from "./discord.svg"
 import styles from "./Footer.module.scss"
 
 export default function Footer() {
     return <footer className={styles.footer}>
-        {/* TODO */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 250">
+            <path
+                fill="var(--g200)"
+                fillOpacity="1"
+                d="M0,128L60,138.7C120,149,240,171,360,160C480,149,600,107,720,85.3C840,64,960,64,1080,90.7C1200,117,1320,171,1380,197.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+            />
+        </svg>
+        <div className={styles.main}>
+            <div className={styles.links}>
+                <Link href="https://github.com/ethteck/decomp.me">
+                    <a className={styles.link}>
+                        <MarkGithubIcon size={24} />
+                        Contribute to decomp.me
+                    </a>
+                </Link>
+
+                <Link href="https://discord.gg/sutqNShRRs">
+                    <a className={styles.link}>
+                        <Discord width={24} />
+                        Chat on Discord
+                    </a>
+                </Link>
+            </div>
+
+            <div className={styles.commitHash}>
+                <Link href={`https://github.com/ethteck/decomp.me/commit/${process.env.NEXT_PUBLIC_COMMIT_HASH}`}>
+                    <a>
+                        {process.env.NEXT_PUBLIC_COMMIT_HASH.slice(0, 7)}
+                    </a>
+                </Link>
+            </div>
+        </div>
     </footer>
 }
