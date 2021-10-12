519945f90a77683db905454d523801a3922b4930
alex <16batesa@gmail.com>
2021-10-12 20:07:47 +0100

    show loading state whilst claiming

--- frontend/src/components/scratch/Scratch.tsx
+++ frontend/src/components/scratch/Scratch.tsx
@@ -120,9 +120,15 @@ export default function Scratch({ slug, tryClaim }: Props) {
     useWarnBeforeUnload(!isSaved, "You have unsaved changes. Are you sure you want to leave?")
 
     // Claim the scratch
-    if (tryClaim && !savedScratch?.owner && !isClaiming && typeof window !== "undefined") {
+    if (tryClaim && !savedScratch?.owner && typeof window !== "undefined") {
+        if (isClaiming) {
+            // Promise that never resolves, since the page will reload when the claim is done
+            throw new Promise(() => {})
+        }
+
         console.log("Claiming scratch", savedScratch)
         isClaiming = true
+
         throw api.post(`/scratch/${scratch.slug}/claim`, {})
             .then(({ success }) => {
                 if (!success)
