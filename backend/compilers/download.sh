a8800b105443695fdf21fdb829bfb38fef74f756
Mark Street <streetster@gmail.com>
2021-10-17 15:28:56 +0100

    PR Feedback

--- backend/compilers/download.sh
+++ backend/compilers/download.sh
@@ -26,4 +26,9 @@ mkdir -p "$compiler_dir/ido7.1"
 curl -L "https://github.com/ethteck/ido-static-recomp/releases/download/master/ido-7.1-recomp-$ido_os-latest.tar.gz" | tar zx -C "$compiler_dir/ido7.1"
 
 # psyq (ps1)
-curl -L "https://github.com/mkst/esa/releases/download/psyq-binaries/psyq-compilers.tar.gz" | tar zx -C "$compiler_dir"
+if [[ "$uname" != "Darwin" ]]; then
+    curl -L "https://github.com/mkst/esa/releases/download/psyq-binaries/psyq-compilers.tar.gz" | tar zx -C "$compiler_dir"
+    curl -L "https://github.com/mkst/esa/releases/download/binutils-2.251/binutils-2.25.1.tar.gz" | tar zx -C $compiler_dir/psyq4.1 usr/local/bin/mips-elf-as --strip-components 3
+    cp $compiler_dir/psyq4.1/mips-elf-as $compiler_dir/psyq4.3/mips-elf-as
+    cp $compiler_dir/psyq4.1/mips-elf-as $compiler_dir/psyq4.6/mips-elf-as
+fi
