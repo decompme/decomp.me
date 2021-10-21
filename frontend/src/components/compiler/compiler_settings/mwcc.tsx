63aca8f6e62600d64b3888d668982d8bb1e7684a
Mark Street <streetster@gmail.com>
2021-10-21 20:42:18 +0100

    Share config between same compiler classes

---
+++ frontend/src/components/compiler/compiler_settings/mwcc.tsx
@@ -0,0 +1,34 @@
+import { Checkbox, FlagSet, FlagOption } from "../CompilerOpts"
+
+export function CommonMWCCFlags() {
+    return <>
+        <FlagSet name="Optimization level">
+            <FlagOption flag="-O0" description="No optimization" />
+            <FlagOption flag="-O1" description="Some optimization" />
+            <FlagOption flag="-O1,p" description="Some optimization + speed" />
+            <FlagOption flag="-O1,s" description="Some optimization + space" />
+            <FlagOption flag="-O2" description="Standard optimization" />
+            <FlagOption flag="-O2,p" description="Standard optimization + speed" />
+            <FlagOption flag="-O2,s" description="Standard optimization + space" />
+            <FlagOption flag="-O3" description="Heavy optimization" />
+            <FlagOption flag="-O3,p" description="Heavy optimization + speed" />
+            <FlagOption flag="-O3,s" description="Heavy optimization + space" />
+            <FlagOption flag="-O4" description="Extreme optimization" />
+            <FlagOption flag="-O4,p" description="Extreme optimization + speed" />
+            <FlagOption flag="-O4,s" description="Extreme optimization + space" />
+        </FlagSet>
+
+        <Checkbox flag="-g" description="Enable debug info" />
+
+        <Checkbox flag="-Cpp_exceptions off" description="-Cpp_exceptions off" />
+        <Checkbox flag="-fp_contract on" description="-fp_contract on" />
+        <Checkbox flag="-msgstyle gcc " description="-msgstyle gcc " />
+        <Checkbox flag="-maxerrors 1" description="-maxerrors 1" />
+        <Checkbox flag="-RTTI off" description="-RTTI off" />
+        <Checkbox flag="-str reuse,pool,readonly" description="-str reuse,pool,readonly" />
+        <Checkbox flag="-char unsigned" description="-char unsigned" />
+        <Checkbox flag="-enum int" description="-enum int" />
+        <Checkbox flag="-use_lmw_stmw on" description="-use_lmw_stmw on" />
+        <Checkbox flag="-inline off" description="-inline off" />
+    </>
+}
--- frontend/src/components/compiler/compilers/psyq4.1.tsx
+++ frontend/src/components/compiler/compilers/psyq4.1.tsx
@@ -1,24 +1,6 @@
-import { Checkbox, FlagSet, FlagOption } from "../CompilerOpts"
+import { CommonPsyqFlags } from "../compiler_settings/psyq"
 
 export const name = "PSYQ4.1 (gcc 2.7.2)"
 export const id = "psyq4.1"
 
-export function Flags() {
-    return <>
-        <FlagSet name="Optimization level">
-            <FlagOption flag="-O0" description="No optimization" />
-            <FlagOption flag="-O1" description="Some optimization" />
-            <FlagOption flag="-O2" description="Standard optimization" />
-            <FlagOption flag="-O3" description="Heavy optimization" />
-        </FlagSet>
-
-        <FlagSet name="Debug information">
-            <FlagOption flag="-g0" description="No debug info" />
-            <FlagOption flag="-g1" description="Minimal trace info" />
-            <FlagOption flag="-g2" description="Local variable tracking" />
-            <FlagOption flag="-g3" description="Macro expansions" />
-        </FlagSet>
-
-        <Checkbox flag="-Wall" description="Enable all warning types" />
-    </>
-}
+export const Flags = () => CommonPsyqFlags()
