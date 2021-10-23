07212580818be8f3c3063fa365c68d2ded705871
Mark Street <22226349+mkst@users.noreply.github.com>
2021-10-23 11:22:32 +0100

    Add GameCube & Wii compiler support (#187)
    * Add Gamecube as a platform
    * Handle flags that include whitespace
    * Share config between same compiler classes

---
+++ frontend/src/components/compiler/compilers/mwcc_233_144.tsx
@@ -0,0 +1,6 @@
+import { CommonMWCCFlags } from "../compiler_settings/mwcc"
+
+export const name = "2.3.3 build 144 (GC MW 1.0)"
+export const id = "mwcc_233_144"
+
+export const Flags = () => CommonMWCCFlags()
