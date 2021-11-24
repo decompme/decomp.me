d0d656c0e0e946e0f0c61c1b3d13cca8a83437d6
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-11-23 20:06:26 -0500

    Delete mwcc_247_108_pikmin2.tsx

--- frontend/src/components/compiler/compilers/mwcc_247_108_pikmin2.tsx
+++
@@ -1,6 +0,0 @@
-import { CommonMWCCFlags } from "../compiler_settings/mwcc"
-
-export const name = "2.4.7 build 108 (GC MW 2.7: pikmin2)"
-export const id = "mwcc_247_108_pikmin2"
-
-export const Flags = () => CommonMWCCFlags()
