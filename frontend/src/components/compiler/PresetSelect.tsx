4274b7a9bc2e440cbbac244653142b54efa8e727
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-12-08 18:27:42 -0500

    add -inline auto to Pikmin 2 preset

--- frontend/src/components/compiler/PresetSelect.tsx
+++ frontend/src/components/compiler/PresetSelect.tsx
@@ -58,7 +58,7 @@ export const PRESETS = [
     {
         name: "Pikmin 2",
         compiler: "mwcc_247_107",
-        opts: "-lang=c++ -nodefaults -Cpp_exceptions off -RTTI off -fp hard -fp_contract on -rostr -O4,p -use_lmw_stmw on -sdata 8 -sdata2 8 -msgstyle gcc",
+        opts: "-lang=c++ -nodefaults -Cpp_exceptions off -RTTI off -fp hard -fp_contract on -rostr -O4,p -use_lmw_stmw on -inline auto -sdata 8 -sdata2 8 -msgstyle gcc",
     },
     {
         name: "Battle for Bikini Bottom",
