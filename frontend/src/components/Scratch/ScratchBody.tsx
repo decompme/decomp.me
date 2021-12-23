e25c9bcab9e989c67bd5df28dd85bbf797e6f8bc
FluentCoding <zSuckyLP@gmail.com>
2021-12-24 00:49:31 +0100

    add ScratchBody props

--- frontend/src/components/Scratch/ScratchBody.tsx
+++ frontend/src/components/Scratch/ScratchBody.tsx
@@ -1,12 +1,28 @@
+import { Dispatch, JSXElementConstructor, ReactElement, RefObject, SetStateAction } from "react"
+
 import * as resizer from "react-simple-resizer"
 
-import Tabs from "../Tabs"
+import { Scratch } from "../../lib/api"
+import { CompilerOptsT } from "../compiler/CompilerOpts"
+import Tabs, { Tab } from "../Tabs"
 
 import ChooseACompiler from "./ChooseACompiler"
 import styles from "./Scratch.module.scss"
 
 const LEFT_PANE_MIN_WIDTH = 400
 const RIGHT_PANE_MIN_WIDTH = 400
+
+export type Props = {
+    container: {width: number | undefined, height: number | undefined, ref: RefObject<HTMLDivElement>}
+    leftTab: string
+    rightTab: string
+    setLeftTab: Dispatch<SetStateAction<string>>
+    setRightTab: Dispatch<SetStateAction<string>>
+    leftTabs: ReactElement<typeof Tab, string | JSXElementConstructor<unknown>>[]
+    rightTabs: ReactElement<typeof Tab, string | JSXElementConstructor<unknown>>[]
+    setCompilerOpts: ({ compiler, compiler_flags }: CompilerOptsT) => void
+    scratch: Scratch
+}
 
 export default function ScratchBody({
     container,
@@ -18,7 +34,7 @@ export default function ScratchBody({
     rightTabs,
     setCompilerOpts,
     scratch,
-}) {
+}: Props) {
     return container.width > (LEFT_PANE_MIN_WIDTH + RIGHT_PANE_MIN_WIDTH)
         ? (<resizer.Container className={styles.resizer}>
             <resizer.Section minSize={LEFT_PANE_MIN_WIDTH}>
