0cac7de9f06281570a00537b2e822d99b9b07f62
Alex Bates <16batesa@gmail.com>
2021-12-24 16:41:10 +0000

    add ErrorBoundary (#250)

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link"
 
+import ErrorBoundary from "../ErrorBoundary"
+
 import Frog from "./frog.svg"
 import LoginState, { Props as LoginStateProps } from "./LoginState"
 import styles from "./Nav.module.scss"
@@ -12,19 +14,23 @@ export type Props = {
 }
 
 export default function Nav({ children, onUserChange }: Props) {
-    return <nav className={styles.nav}>
-        <Link href="/">
-            <a className={styles.logo}>
-                <Frog width={32} height={32} />
-            </a>
-        </Link>
+    return <ErrorBoundary className={styles.nav}>
+        <nav className={styles.nav}>
+            <Link href="/">
+                <a className={styles.logo}>
+                    <Frog width={32} height={32} />
+                </a>
+            </Link>
 
-        {children ?? <Link href="/scratch/new">
-            <a className={styles.item}>New scratch</a>
-        </Link>}
+            {children ?? <Link href="/scratch/new">
+                <a className={styles.item}>New scratch</a>
+            </Link>}
 
-        <div className={styles.grow} />
+            <div className={styles.grow} />
 
-        <LoginState onChange={onUserChange ?? onUserChangeNop} />
-    </nav>
+            <ErrorBoundary>
+                <LoginState onChange={onUserChange ?? onUserChangeNop} />
+            </ErrorBoundary>
+        </nav>
+    </ErrorBoundary>
 }
