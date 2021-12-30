4cca6d68edf5913dedcc141a12a366d6fbe988aa
alex <16batesa@gmail.com>
2021-12-30 12:04:25 +0000

    add Scratch test

---
+++ frontend/src/__mocks__/svgMock.js
@@ -0,0 +1,5 @@
+const React = require("react") // eslint-disable-line
+
+module.exports = function SvgMock() {
+    return React.createElement("svg")
+}
---
+++ frontend/src/components/Scratch/Scratch.test.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+import { act, render } from "@testing-library/react"
+
+import * as api from "../../lib/api"
+
+import Scratch from "./Scratch"
+
+const scratchJson: api.Scratch = {
+    "slug": "test",
+    "name": "",
+    "description": "I am the description",
+    "compiler": "ido5.3",
+    "platform": "mips",
+    "compiler_flags": "-O2 -fforce-addr",
+    "source_code": "void func(void) {\n    // ...\n}",
+    "context": "",
+    "owner": {
+        "is_you": true,
+        "is_anonymous": true,
+    },
+    "parent": null,
+    "diff_label": "",
+    "score": 0,
+}
+
+test("renders without causing a state change", async () => {
+    const onChange = jest.fn()
+
+    act(() => {
+        render(<Scratch
+            scratch={scratchJson}
+            isSaved
+            onChange={onChange}
+        />)
+    })
+
+    expect(onChange).toHaveBeenCalledTimes(0)
+})
