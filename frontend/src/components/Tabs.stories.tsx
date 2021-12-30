116323a9e09796f6744112e60a367a25e56854e6
Ethan Roseman <ethteck@gmail.com>
2021-12-30 11:56:05 -0500

    Fix build (#264)

--- frontend/src/components/Tabs.stories.tsx
+++ frontend/src/components/Tabs.stories.tsx
@@ -9,16 +9,16 @@ export default {
 
 export const Basic: ComponentStory<typeof Tabs> = args => {
     return <Tabs {...args}>
-        <Tab key="Tab 1">
+        <Tab tabKey="tab 1" key="Tab 1">
             Tab 1 content
         </Tab>
-        <Tab key="Tab 2">
+        <Tab tabKey="tab 2" key="Tab 2">
             Tab 2 content
         </Tab>
-        <Tab key="Disabled tab" disabled>
+        <Tab tabKey="tab 3" key="Disabled tab" disabled>
             How did you get here?
         </Tab>
-        <Tab key="Tab 4">
+        <Tab tabKey="tab 4" key="Tab 4">
             Tab 4 content
         </Tab>
     </Tabs>
