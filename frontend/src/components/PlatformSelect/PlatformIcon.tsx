75eefc3a68cd92f69e859264fcee2d842e18c17a
Ethan Roseman <ethteck@gmail.com>
2022-01-01 13:21:14 -0500

    Initial GBA support (#278)
    * Initial GBA support
    
    * fix

--- frontend/src/components/PlatformSelect/PlatformIcon.tsx
+++ frontend/src/components/PlatformSelect/PlatformIcon.tsx
@@ -1,3 +1,4 @@
+import LogoGBA from "./gba.svg"
 import LogoGCWii from "./gc_wii.svg"
 import LogoN64 from "./n64.svg"
 import LogoNDS from "./nds.svg"
@@ -6,6 +7,7 @@ import LogoPS2 from "./ps2.svg"
 import UnknownIcon from "./unknown.svg"
 
 const ICONS = {
+    "gba": LogoGBA,
     "gc_wii": LogoGCWii,
     "nds_arm9": LogoNDS,
     "n64": LogoN64,
