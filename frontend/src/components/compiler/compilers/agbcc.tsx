b062100556c7a4de38563545ade3354072b95954
Ethan Roseman <ethteck@gmail.com>
2022-01-15 00:19:18 +0900

    one file

--- frontend/src/components/compiler/compilers/agbcc.tsx
+++ frontend/src/components/compiler/compilers/agbcc.tsx
@@ -1,25 +1,6 @@
-import { Checkbox, FlagSet, FlagOption } from "../CompilerOpts"
+import { CommonAgbccFlags } from "../compiler_settings/agbcc"
 
 export const name = "agbcc"
 export const id = "agbcc"
 
-export function Flags() {
-    return <>
-        <FlagSet name="Optimization level">
-            <FlagOption flag="-O0" description="No optimization" />
-            <FlagOption flag="-O1" description="Some optimization" />
-            <FlagOption flag="-O2" description="Heavy optimization" />
-        </FlagSet>
-
-        <FlagSet name="Debug information">
-            <FlagOption flag="-g0" description="No debug info" />
-            <FlagOption flag="-g1" description="Minimal trace info" />
-            <FlagOption flag="-g2" description="Local variable tracking" />
-            <FlagOption flag="-g3" description="Macro expansions" />
-        </FlagSet>
-
-        <Checkbox flag="-fforce-addr" description="Load pointers into registers before use" />
-
-        <Checkbox flag="-Wall" description="Enable all warning types" />
-    </>
-}
+export const Flags = () => CommonAgbccFlags()
