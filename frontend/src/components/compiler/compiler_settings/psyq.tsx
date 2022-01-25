8e74bf89216cac1ee4f3475b0203057fdc88cc90
Mark Street <22226349+mkst@users.noreply.github.com>
2022-01-25 06:12:26 +0000

    Update psx psyq presets to avoid hardcoding -G flag (#324)

--- frontend/src/components/compiler/compiler_settings/psyq.tsx
+++ frontend/src/components/compiler/compiler_settings/psyq.tsx
@@ -7,6 +7,7 @@ export function CommonPsyqFlags() {
             <FlagOption flag="-O1" description="Some optimization" />
             <FlagOption flag="-O2" description="Standard optimization" />
             <FlagOption flag="-O3" description="Heavy optimization" />
+            <FlagOption flag="-Os" description="Size optimization" />
         </FlagSet>
 
         <FlagSet name="Debug information">
@@ -14,6 +15,11 @@ export function CommonPsyqFlags() {
             <FlagOption flag="-g1" description="Minimal trace info" />
             <FlagOption flag="-g2" description="Local variable tracking" />
             <FlagOption flag="-g3" description="Macro expansions" />
+        </FlagSet>
+
+        <FlagSet name="Small data limit">
+            <FlagOption flag="-G0" description="0 bytes" />
+            <FlagOption flag="-G8" description="8 bytes" />
         </FlagSet>
 
         <Checkbox flag="-Wall" description="Enable all warning types" />
