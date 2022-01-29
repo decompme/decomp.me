74b0f41f1bffbaa52e7273b827d47c162ad446f2
notyourav <65437533+notyourav@users.noreply.github.com>
2022-01-28 23:43:35 -0800

    fix gba thumb macro

--- backend/coreapp/compiler_wrapper.py
+++ backend/coreapp/compiler_wrapper.py
@@ -296,12 +296,18 @@ def load_platforms() -> Dict[str, Platform]:
 .endm
 
 .macro arm_func_start name
+    .arm
+    \name:
 .endm
 .macro arm_func_end name
 .endm
 .macro thumb_func_start name
+    .thumb
+    \name:
 .endm
 .macro non_word_aligned_thumb_func_start name
+    .thumb
+    \name:
 .endm
 .macro thumb_func_end name
 .endm
