eb6f391e6c606ca6f545d5ca2629e86839dba8c9
Luciano Ciccariello <xeeynamo@hotmail.com>
2022-02-10 16:29:18 +0000

    Tweak GCC flags

--- frontend/src/components/compiler/compilers/gcc2.6.3-mipsel.tsx
+++ frontend/src/components/compiler/compilers/gcc2.6.3-mipsel.tsx
@@ -1,6 +1,17 @@
+import { FlagOption, FlagSet } from "../CompilerOpts";
 import { CommonGccFlags } from "../compiler_settings/gcc"
 
 export const name = "GCC 2.6.3 (mipsel)"
 export const id = "gcc2.6.3-mipsel"
 
-export const Flags = () => CommonGccFlags()
+export function Flags() {
+    return <>
+        { CommonGccFlags() }
+
+        <FlagSet name="Small data limit">
+            <FlagOption flag="-G0" description="0 bytes" />
+            <FlagOption flag="-G4" description="4 bytes" />
+            <FlagOption flag="-G8" description="8 bytes" />
+        </FlagSet>
+    </>
+}
