60762f2cc928fa62e8a5f72080601f40b7c2e3ae
Mark Street <streetster@gmail.com>
2022-02-21 13:01:35 +0000

    fixup PR comments

--- backend/coreapp/migrations/0017_rename_psyq43_compiler.py
+++ backend/coreapp/migrations/0017_rename_psyq43_compiler.py
@@ -4,13 +4,13 @@ import django.db.migrations.operations.special
 
 def rename_psyq43(apps, schema_editor):
     """
-    Restore forks' projects
+    Psyq4.3 no longer exists, its been replaced with gcc2.7.2-psyq
     """
     Scratch = apps.get_model("coreapp", "Scratch")
     for row in Scratch.objects.all():
         if row.compiler == "psyq4.3":
             row.compiler = "gcc2.7.2-psyq"
-        row.save()  # type: ignore
+            row.save(update_fields=["compiler"])
 
 
 class Migration(migrations.Migration):
