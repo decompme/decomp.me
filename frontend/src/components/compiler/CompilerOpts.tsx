f0d018bd82838ded265ac9b55699bfe0bf440bae
angie <angheloalf95@gmail.com>
2022-04-01 15:41:26 -0300

    objdump section is a bit more independent from the compilerargs one

--- frontend/src/components/compiler/CompilerOpts.tsx
+++ frontend/src/components/compiler/CompilerOpts.tsx
@@ -63,6 +63,7 @@ export function FlagOption({ flag, description }: { flag: string, description?: 
 export type CompilerOptsT = {
     compiler: string
     compiler_flags: string
+    objdump_flags: string
     preset: string
 }
 
@@ -76,11 +77,13 @@ export type Props = {
 export default function CompilerOpts({ platform, value, onChange, isPopup }: Props) {
     const compiler = value.compiler
     let opts = value.compiler_flags
+    let objdump_opts = value.objdump_flags
 
     const setCompiler = (compiler: string) => {
         onChange({
             compiler,
             compiler_flags: opts,
+            objdump_flags: objdump_opts,
             preset: "",
         })
     }
@@ -89,6 +92,16 @@ export default function CompilerOpts({ platform, value, onChange, isPopup }: Pro
         onChange({
             compiler,
             compiler_flags: opts,
+            objdump_flags: objdump_opts,
+            preset: "",
+        })
+    }
+
+    const setObjdumpOpts = (objdump_opts: string) => {
+        onChange({
+            compiler,
+            compiler_flags: opts,
+            objdump_flags: objdump_opts,
             preset: "",
         })
     }
@@ -97,36 +110,58 @@ export default function CompilerOpts({ platform, value, onChange, isPopup }: Pro
         onChange({
             compiler: preset.compiler,
             compiler_flags: preset.flags,
+            objdump_flags: objdump_opts,
             preset: preset.name,
         })
     }
 
-    return <OptsContext.Provider value={{
-        checkFlag(flag: string) {
-            return (" " + opts + " ").includes(" " + flag + " ")
-        },
+    return <div>
+        <OptsContext.Provider value={{
+            checkFlag(flag: string) {
+                return (" " + opts + " ").includes(" " + flag + " ")
+            },
 
-        setFlag(flag: string, enable: boolean) {
-            if (enable) {
-                opts = opts + " " + flag
-            } else {
-                opts = (" " + opts + " ").replace(" " + flag + " ", " ")
-            }
-            opts = opts.trim()
-            setOpts(opts)
-        },
-    }}>
-        <div className={styles.header} data-is-popup={isPopup}>
-            <PlatformIcon platform={platform} size={32} />
-            <div className={styles.preset}>
-                Preset
-                <PresetSelect platform={platform} presetName={value.preset} setPreset={setPreset} />
+            setFlag(flag: string, enable: boolean) {
+                if (enable) {
+                    opts = opts + " " + flag
+                } else {
+                    opts = (" " + opts + " ").replace(" " + flag + " ", " ")
+                }
+                opts = opts.trim()
+                setOpts(opts)
+            },
+        }}>
+            <div className={styles.header} data-is-popup={isPopup}>
+                <PlatformIcon platform={platform} size={32} />
+                <div className={styles.preset}>
+                    Preset
+                    <PresetSelect platform={platform} presetName={value.preset} setPreset={setPreset} />
+                </div>
             </div>
-        </div>
-        <div className={styles.container} data-is-popup={isPopup}>
-            <OptsEditor platform={platform} compiler={compiler} setCompiler={setCompiler} opts={opts} setOpts={setOpts} />
-        </div>
-    </OptsContext.Provider>
+            <div className={styles.container} data-is-popup={isPopup}>
+                <OptsEditor platform={platform} compiler={compiler} setCompiler={setCompiler} opts={opts} setOpts={setOpts} />
+            </div>
+        </OptsContext.Provider>
+        <OptsContext.Provider value={{
+            checkFlag(flag: string) {
+                return (" " + objdump_opts + " ").includes(" " + flag + " ")
+            },
+
+            setFlag(flag: string, enable: boolean) {
+                if (enable) {
+                    objdump_opts = objdump_opts + " " + flag
+                } else {
+                    objdump_opts = (" " + objdump_opts + " ").replace(" " + flag + " ", " ")
+                }
+                objdump_opts = objdump_opts.trim()
+                setObjdumpOpts(objdump_opts)
+            },
+        }}>
+            <div className={styles.container} data-is-popup={isPopup}>
+                <ObjdumpOptsEditor platform={platform} compiler={compiler} objdump_opts={objdump_opts} setObjdumpOpts={setObjdumpOpts} />
+            </div>
+        </OptsContext.Provider>
+    </div>
 }
 
 export function OptsEditor({ platform, compiler: compilerId, setCompiler, opts, setOpts }: {
@@ -174,13 +209,33 @@ export function OptsEditor({ platform, compiler: compilerId, setCompiler, opts, 
         <div className={styles.flags}>
             {(compilerId && compiler) ? <CompilerFlags schema={compiler.flags} /> : <div />}
         </div>
+    </div>
+}
 
+export function ObjdumpOptsEditor({ platform, compiler: compilerId, objdump_opts, setObjdumpOpts }: {
+    platform?: string
+    compiler: string
+    objdump_opts: string
+    setObjdumpOpts: (objdump_opts: string) => void
+}) {
+    const compilers = useCompilersForPlatform(platform)
+    const compiler = compilers[compilerId]
+
+    return <div>
         <div className={styles.row}>
-            Objdump flags
-
-            <div className={styles.objdumpFlags}>
-                {(compilerId && compiler) ? <CompilerFlags schema={compiler.objdump_flags} /> : <div />}
+            <div className={styles.preset}>
+                objdump flags
             </div>
+            <input
+                type="text"
+                className={styles.textbox}
+                value={objdump_opts}
+                placeholder="no arguments"
+                onChange={e => setObjdumpOpts((e.target as HTMLInputElement).value)}
+            />
+        </div>
+        <div className={styles.objdumpFlags}>
+            {(compilerId && compiler) ? <CompilerFlags schema={compiler.objdump_flags} /> : <div />}
         </div>
     </div>
 }
