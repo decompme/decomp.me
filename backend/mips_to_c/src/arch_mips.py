da5446edd162d4bc8791fd304e131066f65a9780
Ethan Roseman <ethteck@gmail.com>
2022-04-02 00:18:28 +0900

    Tool updates (#426)
    * git subrepo pull --force backend/asm_differ
    
    subrepo:
      subdir:   "backend/asm_differ"
      merged:   "d591a164"
    upstream:
      origin:   "https://github.com/simonlindholm/asm-differ"
      branch:   "main"
      commit:   "d591a164"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"
    
    * git subrepo pull --force backend/mips_to_c
    
    subrepo:
      subdir:   "backend/mips_to_c"
      merged:   "942633a5"
    upstream:
      origin:   "https://github.com/matt-kempster/mips_to_c"
      branch:   "master"
      commit:   "942633a5"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

--- backend/mips_to_c/src/arch_mips.py
+++ backend/mips_to_c/src/arch_mips.py
@@ -288,14 +288,32 @@ class Div2S16Pattern(SimpleAsmPattern):
         return Replacement([m.body[0], m.body[1], div], len(m.body))
 
 
-class Div2S32Pattern(SimpleAsmPattern):
+class Div2S32Pattern1(SimpleAsmPattern):
     pattern = make_pattern(
-        "srl $o, $i, 0x1f",
-        "addu $o, $i, $o",
-        "sra $o, $o, 1",
+        "srl $t, $i, 0x1f",
+        "addu $t, $i, $t",
+        "sra $o, $t, 1",
     )
 
-    def replace(self, m: AsmMatch) -> Replacement:
+    def replace(self, m: AsmMatch) -> Optional[Replacement]:
+        if m.regs["t"] == m.regs["i"]:
+            return None
+        div = AsmInstruction("div.fictive", [m.regs["o"], m.regs["i"], AsmLiteral(2)])
+        # While it would be more correct, we don't include m.body[:2] in the
+        # result, because the srl incorrectly type inferences $i to u32.
+        return Replacement([div], len(m.body))
+
+
+class Div2S32Pattern2(SimpleAsmPattern):
+    pattern = make_pattern(
+        "srl $t, $i, 0x1f",
+        "addu $i, $i, $t",
+        "sra $o, $i, 1",
+    )
+
+    def replace(self, m: AsmMatch) -> Optional[Replacement]:
+        if m.regs["t"] == m.regs["i"]:
+            return None
         div = AsmInstruction("div.fictive", [m.regs["o"], m.regs["i"], AsmLiteral(2)])
         return Replacement([div], len(m.body))
 
@@ -567,7 +585,7 @@ class MipsArch(Arch):
         "fs5f": Register("f31"),
     }
 
-    aliased_regs = { **o32abi_float_regs, **aliased_gp_regs }
+    aliased_regs = {**o32abi_float_regs, **aliased_gp_regs}
 
     @classmethod
     def missing_return(cls) -> List[Instruction]:
@@ -911,7 +929,8 @@ class MipsArch(Arch):
         DivP2Pattern1(),
         DivP2Pattern2(),
         Div2S16Pattern(),
-        Div2S32Pattern(),
+        Div2S32Pattern1(),
+        Div2S32Pattern2(),
         ModP2Pattern1(),
         ModP2Pattern2(),
         UtfPattern(),
