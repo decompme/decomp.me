d05339ceb81c8a6c4c8d9a1b8c0f43d778d9dc9b
Alex Bates <alex@nanaian.town>
2022-04-13 13:43:56 +0100

    use User-Agent Client Hints API if supported

--- frontend/src/components/Shortcut.tsx
+++ frontend/src/components/Shortcut.tsx
@@ -1,6 +1,21 @@
 import { useEffect, useState } from "react"
 
-const isMacOS = typeof window !== "undefined" && window.navigator.userAgent.includes("Mac OS X")
+function isMacOS(): boolean {
+    if (typeof window === "undefined") {
+        // SSR
+        return false
+    }
+
+    // Use User-Agent Client Hints API if supported
+    // @ts-ignore
+    if (navigator.userAgentData) {
+        // @ts-ignore
+        return navigator.userAgentData.platform == "macOS"
+    }
+
+    // Fall back to user-agent sniffing
+    return navigator.userAgent.includes("Mac OS X")
+}
 
 export type Key = string | SpecialKey
 
