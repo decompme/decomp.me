fa9bab2ec61531cbaac163fcffc2b91242b4cbc4
Ethan Roseman <ethteck@gmail.com>
2022-09-03 12:18:11 +0900

    revert CompilationPanel changes

--- frontend/src/components/Diff/CompilationPanel.tsx
+++ frontend/src/components/Diff/CompilationPanel.tsx
@@ -1,16 +1,12 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 
-import { ChevronDownIcon } from "@primer/octicons-react"
 import Ansi from "ansi-to-react"
-import classNames from "classnames"
 import * as resizer from "react-simple-resizer"
 
 import * as api from "../../lib/api"
 
 import styles from "./CompilationPanel.module.scss"
 import Diff from "./Diff"
-
-const CLOSED_BOTTOM_PANEL_SIZE = 35
 
 function getProblemState(compilation: api.Compilation): ProblemState {
     if (compilation.diff_output) {
@@ -40,24 +36,13 @@ export type Props = {
 export default function CompilationPanel({ compilation, isCompiling, isCompilationOld, selectedSourceLine }: Props) {
     const [diff, setDiff] = useState<api.DiffOutput | null>(null)
     const problemState = getProblemState(compilation)
-    const resizerContainer = useRef<resizer.Container>(null)
 
     useEffect(() => {
         if (compilation.diff_output)
             setDiff(compilation.diff_output)
     }, [compilation.diff_output])
 
-    const toggleBottomPanel = () => {
-        const resizer = resizerContainer.current.getResizer()
-        if (resizer.getSectionSize(1) == CLOSED_BOTTOM_PANEL_SIZE) {
-            resizer.resizeSection(1, { toSize: 300 })
-        } else {
-            resizer.resizeSection(1, { toSize: CLOSED_BOTTOM_PANEL_SIZE })
-        }
-        resizerContainer.current.applyResizer(resizer)
-    }
-
-    return <resizer.Container vertical ref={resizerContainer} className={styles.container}>
+    return <resizer.Container vertical className={styles.container}>
         <resizer.Section minSize={100}>
             <Diff
                 diff={diff}
@@ -68,23 +53,14 @@ export default function CompilationPanel({ compilation, isCompiling, isCompilati
         </resizer.Section>
         <resizer.Bar
             size={1}
-            className={classNames(styles.bar, { [styles.noPointerEvents]: problemState == ProblemState.NO_PROBLEMS })}
+            className={styles.bar}
             expandInteractiveArea={{ top: 2, bottom: 2 }}
         />
-        <resizer.Section
-            className={styles.problems}
-            maxSize={problemState == ProblemState.NO_PROBLEMS ? CLOSED_BOTTOM_PANEL_SIZE : undefined}
-            minSize={CLOSED_BOTTOM_PANEL_SIZE}
-        >
-            {problemState == ProblemState.NO_PROBLEMS ? <h2>
-                Compiled successfully
-            </h2> : <h2 onClick={toggleBottomPanel} className={styles.bottomPanelClickable}>
-                {problemState == ProblemState.ERRORS ? "Compilation failed" : "Compiled with warnings"}
-                <ChevronDownIcon />
-            </h2>}
-            {problemState != ProblemState.NO_PROBLEMS && <div className={styles.log}>
+        {(problemState != ProblemState.NO_PROBLEMS) && <resizer.Section className={styles.problems} minSize={100}>
+            <h2>Compiler {problemState == ProblemState.ERRORS ? "errors" : "warnings"}</h2>
+            <div className={styles.log}>
                 <Ansi>{compilation.errors}</Ansi>
-            </div>}
-        </resizer.Section>
+            </div>
+        </resizer.Section>}
     </resizer.Container>
 }
