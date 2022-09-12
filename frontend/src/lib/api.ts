95d639d6708f697ce0a32e6933fbabab8204d9be
Alex Bates <alex@nanaian.town>
2022-09-12 17:12:41 +0100

    patch over diff_flags type issue

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -444,7 +444,7 @@ export function useIsScratchSaved(scratch: Scratch): boolean {
         scratch.description === saved.description &&
         scratch.compiler === saved.compiler &&
         scratch.compiler_flags === saved.compiler_flags &&
-        scratch.diff_flags.join(",") === saved.diff_flags.join(",") &&
+        JSON.stringify(scratch.diff_flags) === JSON.stringify(saved.diff_flags) &&
         scratch.diff_label === saved.diff_label &&
         scratch.source_code === saved.source_code &&
         scratch.context === saved.context
