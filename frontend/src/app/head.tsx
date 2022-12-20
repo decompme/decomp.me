cb1e31edd0af89b16fba4185dab90f2c1609a11b
Alex Bates <alex@nanaian.town>
2022-12-20 00:55:52 +0000

    fix app head

--- frontend/src/app/head.tsx
+++ frontend/src/app/head.tsx
@@ -2,9 +2,17 @@ export default function Head() {
     return (
         <>
             <title>decomp.me</title>
-            <meta content="width=device-width, initial-scale=1" name="viewport" />
-            <link rel="icon" href="/favicon.ico" />
+
+            <meta charSet="utf-8" />
+
             <meta name="theme-color" content="#282e31" />
+            <meta name="viewport" content="width=device-width" />
+            <meta name="darkreader-lock" />
+
+            <link rel="manifest" href="/manifest.json" />
+
+            <link rel="shortcut icon" href="/purplefrog.svg" />
+            <link rel="apple-touch-icon" href="/purplefrog-bg-180.png" />
         </>
     )
 }
