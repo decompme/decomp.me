9e75e2ce112b42a3bc0e81de7c523c68bcc61079
Alex Bates <alex@nanaian.town>
2022-12-21 16:41:01 +0000

    fix Logotype aria issue

--- frontend/src/components/Logotype.tsx
+++ frontend/src/components/Logotype.tsx
@@ -1,8 +1,8 @@
 import Frog from "./Nav/frog.svg"
 
 export default function Logotype() {
-    return <div className="inline-flex items-center space-x-2">
-        <Frog className="h-7 w-7" />
+    return <div className="inline-flex items-center space-x-2" aria-label="decomp.me logo">
+        <Frog className="h-7 w-7" aria-label="Purple frog" />
         <span className="text-xl font-semibold leading-6 tracking-tight">decomp.me</span>
     </div>
 }
