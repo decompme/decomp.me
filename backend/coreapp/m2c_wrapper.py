0c860952879358c6f3589927326793b049f51d10
ConorBobbleHat <ConorBobbleHat>
2022-12-23 00:54:48 +0000

    Use dill serialization to enable support for timeouts on windows

--- backend/coreapp/m2c_wrapper.py
+++ backend/coreapp/m2c_wrapper.py
@@ -7,6 +7,7 @@ from m2c.src.main import parse_flags, run
 from coreapp.compilers import Compiler
 
 from coreapp.sandbox import Sandbox
+from coreapp.util import exception_on_timeout
 
 logger = logging.getLogger(__name__)
 
@@ -37,6 +38,7 @@ class M2CWrapper:
         return f"{t_arch}-{t_compiler}"
 
     @staticmethod
+    @exception_on_timeout(timeout_seconds=5)
     def decompile(asm: str, context: str, compiler: Compiler, arch: str) -> str:
         with Sandbox() as sandbox:
             flags = ["--stop-on-error", "--pointer-style=left"]
--- backend/coreapp/util.py
+++ backend/coreapp/util.py
@@ -1,7 +1,13 @@
 import hashlib
 import logging
 import time
-from typing import Tuple
+
+import multiprocessing
+import functools
+import dill  # type: ignore
+
+from typing import Tuple, TypeVar, Callable, Any, cast
+from queue import Queue
 
 logger = logging.getLogger(__name__)
 
@@ -11,3 +17,48 @@ logger.info("Startup time: %s", _startup_time)
 
 def gen_hash(key: Tuple[str, ...]) -> str:
     return hashlib.sha256(str(key + (_startup_time,)).encode("utf-8")).hexdigest()
+
+
+F = TypeVar("F", bound=Callable[..., Any])
+
+# Python 3.10+ should allow this to be typed more concretely
+# (see https://mypy.readthedocs.io/en/stable/generics.html#declaring-decorators)
+
+# Windows requires multiprocessing processes to be in top-level scope
+def worker(queue: Queue[Any], func: bytes, args: Any, kwargs: Any) -> Any:
+    try:
+        ret = dill.loads(func)(*args, **kwargs)
+        queue.put(ret)
+    except Exception as e:
+        queue.put(e)
+
+
+def exception_on_timeout(timeout_seconds: float = 5) -> Callable[[F], F]:
+    def timeout_inner(func: F) -> F:
+        @functools.wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            queue: Queue[Any] = multiprocessing.Queue()
+
+            # On Windows, multiprocessing uses pickle under the hood to serialize arguments
+            # It doesn't play nicely with arbitary functions, so we explicitly use its
+            # more versatile cousin (dill) to handle the serialization ourselves
+            p = multiprocessing.Process(
+                target=worker, args=(queue, dill.dumps(func), args, kwargs)
+            )
+            p.start()
+            p.join(timeout_seconds)
+
+            if p.is_alive():
+                # The process has hanged - terminate, and throw an error
+                p.terminate()
+                p.join()
+                raise TimeoutError("Process timed out")
+            else:
+                ret = queue.get()
+                if isinstance(ret, Exception):
+                    raise ret
+                return ret
+
+        return cast(F, wrapper)
+
+    return timeout_inner
