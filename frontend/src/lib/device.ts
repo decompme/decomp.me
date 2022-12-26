a6cd2fa9fbec8ebd818c2b84c198d6e55ecaf1e9
Alex Bates <alex@nanaian.town>
2022-12-26 17:28:47 +0000

    implement isMacOS for SSR

--- frontend/src/lib/device.ts
+++ frontend/src/lib/device.ts
@@ -1,16 +1,25 @@
-export function isMacOS(): boolean {
-    if (typeof window === "undefined") {
-        // SSR
-        return false
-    }
+import { headers } from "next/headers"
 
+export function isSSR(): boolean {
+    return typeof window === "undefined"
+}
+
+export function getUserAgent(): string {
+    if (isSSR()) {
+        return headers().get("user-agent") ?? ""
+    } else {
+        return navigator.userAgent
+    }
+}
+
+export function isMacOS(): boolean {
     // Use User-Agent Client Hints API if supported
     // @ts-ignore
-    if (navigator.userAgentData) {
+    if (!isSSR() && navigator.userAgentData) {
         // @ts-ignore
         return navigator.userAgentData.platform == "macOS"
     }
 
     // Fall back to user-agent sniffing
-    return navigator.userAgent.includes("Mac OS X")
+    return getUserAgent().includes("Mac OS X")
 }
