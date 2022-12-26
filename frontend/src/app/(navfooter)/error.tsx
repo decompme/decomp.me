8f4af180596954705b1d83210584b8df46d6ddd2
Alex Bates <alex@nanaian.town>
2022-12-26 15:36:40 +0000

    improve error pages

---
+++ frontend/src/app/(navfooter)/error.tsx
@@ -0,0 +1,5 @@
+// Rexport app error page to include the layout
+
+import Error from "../error"
+
+export default Error
--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -6,6 +6,7 @@ import { SyncIcon } from "@primer/octicons-react"
 
 import Button from "@/components/Button"
 import ErrorBoundary from "@/components/ErrorBoundary"
+import SetPageTitle from "@/components/SetPageTitle"
 
 export default function Error({
     error,
@@ -18,21 +19,26 @@ export default function Error({
         console.error(error)
     }, [error])
 
-    return <main className="max-w-prose p-4 md:mx-auto">
-        <h1 className="text-3xl font-semibold">Something went wrong</h1>
-        <p className="py-4">
-            An unexpected error occurred rendering this page.
-        </p>
-        <div className="rounded bg-gray-9 p-4 text-gray-2">
-            <code className="font-mono text-sm">{error.toString()}</code>
-        </div>
-        <p className="py-4">
-            If this keeps happening, <a href="https://discord.gg/sutqNShRRs" className="text-blue-5 hover:underline active:translate-y-px">let us know</a>.
-        </p>
-        <ErrorBoundary>
-            <Button onClick={reset}>
-                <SyncIcon /> Try again
-            </Button>
-        </ErrorBoundary>
-    </main>
+    return <>
+        <SetPageTitle title="Error" />
+        <div className="grow" />
+        <main className="max-w-prose p-4 md:mx-auto">
+            <h1 className="text-3xl font-semibold">Something went wrong</h1>
+            <p className="py-4">
+                An unexpected error occurred rendering this page.
+            </p>
+            <div className="rounded bg-gray-2 p-4 text-gray-11">
+                <code className="font-mono text-sm">{error.toString()}</code>
+            </div>
+            <p className="py-4">
+                If this keeps happening, <a href="https://discord.gg/sutqNShRRs" className="text-blue-11 hover:underline active:translate-y-px">let us know</a>.
+            </p>
+            <ErrorBoundary>
+                <Button onClick={reset}>
+                    <SyncIcon /> Try again
+                </Button>
+            </ErrorBoundary>
+        </main>
+        <div className="grow" />
+    </>
 }
