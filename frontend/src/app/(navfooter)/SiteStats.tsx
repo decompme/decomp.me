78ae0c07fb6605b705246983e7eb5fdd2d093be1
Alex Bates <alex@nanaian.town>
2022-12-26 16:45:16 +0000

    link to stats

--- frontend/src/app/(navfooter)/SiteStats.tsx
+++ frontend/src/app/(navfooter)/SiteStats.tsx
@@ -2,9 +2,17 @@
 
 import { ReactNode } from "react"
 
+import Link from "next/link"
+
 import { useStats } from "@/lib/api"
 
-function Stat({ children }: { children: ReactNode }) {
+function Stat({ children, href }: { children: ReactNode, href?: string }) {
+    if (href) {
+        return <Link href={href} className="hover:underline active:translate-y-px">
+            {children}
+        </Link>
+    }
+
     return <span>
         {children}
     </span>
@@ -19,7 +27,7 @@ export default function SiteStats() {
 
     return <p className="inline-flex gap-8 text-xs text-gray-11 md:gap-16">
         <Stat>{stats.scratch_count.toLocaleString()} scratches created</Stat>
-        <Stat>{stats.profile_count.toLocaleString()} unique visitors</Stat>
+        <Stat href="https://stats.decomp.me/decomp.me">{stats.profile_count.toLocaleString()} unique visitors</Stat>
         <Stat>{stats.github_user_count.toLocaleString()} users signed up</Stat>
         <Stat>{stats.asm_count.toLocaleString()} asm globs submitted</Stat>
     </p>
