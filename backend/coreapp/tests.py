4d6494b870931664ee209b3930ddc6fa1d672588
ConorBobbleHat <c.github@firstpartners.net>
2023-01-20 17:27:37 +0000

    black

--- backend/coreapp/tests.py
+++ backend/coreapp/tests.py
@@ -585,7 +585,8 @@ class TimeoutTests(BaseTestCase):
             self.assertFalse(response.json()["success"])
             self.assertIn("timeout expired", response.json()["compiler_output"].lower())
 
-    @requiresCompiler(compilers.DUMMY_LONGRUNNING) # if we don't have DUMMY_LONGRUNNING, it means we'll be unable to use sandbox.run_subprocess
+    # if we don't have DUMMY_LONGRUNNING, it means we'll be unable to use sandbox.run_subprocess
+    @requiresCompiler(compilers.DUMMY_LONGRUNNING)
     def test_zero_timeout(self) -> None:
         # Tests that passing a timeout of zero to sandbox.run_subprocess will equate
         # to disabling the timeout entirely
