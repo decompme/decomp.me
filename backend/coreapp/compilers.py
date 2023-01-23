6f518ae49a28390165e3712583c4ac731fa4d72f
angie <angheloalf95@gmail.com>
2023-01-22 23:38:09 -0300

    whoopsie

--- backend/coreapp/compilers.py
+++ backend/coreapp/compilers.py
@@ -401,7 +401,7 @@ GCC281SNCXX = GCCCompiler(
 EGCS1124 = GCCCompiler(
     id="egcs_1.1.2-4",
     platform=N64,
-    cc='COMPILER_PATH="${COMPILER_DIR}" "${COMPILER_DIR}"/mips-linux-gcc -c -G 0 -fno-PIC -mgp32 -mfp32 -mcpu=4300 -mips3 -nostdinc ${COMPILER_FLAGS} "${INPUT}" -o "${OUTPUT}"',
+    cc='COMPILER_PATH="${COMPILER_DIR}" "${COMPILER_DIR}"/mips-linux-gcc -c -G 0 -fno-PIC -mgp32 -mfp32 -mcpu=4300 -nostdinc ${COMPILER_FLAGS} "${INPUT}" -o "${OUTPUT}"',
 )
 
 # MACOS9
@@ -898,18 +898,18 @@ _all_presets = [
     Preset("Diddy Kong Racing", IDO53, "-O2 -mips1"),
     Preset("Dinosaur Planet", IDO53, "-O2 -g3 -mips2"),
     Preset("Dinosaur Planet (DLLs)", IDO53, "-O2 -g3 -mips2 -KPIC"),
-    Preset("Dr. Mario 64 US", GCC272KMC, "-O2 -mips2", diff_flags=["-Mreg-names=32"]),
+    Preset("Dr. Mario 64 US", GCC272KMC, "-O2 -mips3", diff_flags=["-Mreg-names=32"]),
     Preset(
         "Dr. Mario 64 CN",
         EGCS1124,
-        "-O2 -g -mips3 -mcpu=4300 -funsigned-char",
+        "-O2 -g -mips2 -mcpu=4300 -funsigned-char",
         diff_flags=["-Mreg-names=32"],
     ),
     Preset("GoldenEye / Perfect Dark", IDO53, "-Olimit 2000 -mips2 -O2"),
     Preset(
         "libultra iQue",
         EGCS1124,
-        "-mcpu=4300 -mno-abicalls -mips2 -O2",
+        "-O2 -mips2 -mcpu=4300 -mno-abicalls",
         diff_flags=["-Mreg-names=32"],
     ),
     Preset(
