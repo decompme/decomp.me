eae98bcaaa984f41b1e0a070f85abc385e6e8bb9
LLONSIT <lonsitsyt456@gmail.com>
2023-02-09 18:23:22 -0500

    impl

--- backend/coreapp/compilers.py
+++ backend/coreapp/compilers.py
@@ -358,7 +358,6 @@ if os.environ.get("RUNNING_IN_DOCKER") == "1":
    #     cc='${QEMU_IRIX} -L /compilers/ido6.0 "${COMPILER_DIR}/usr/bin/cc" -c -Xcpluscomm -G0 -non_shared -Wab,-r4300_mul -woff 649,838,712 -32 ${COMPILER_FLAGS} -o "${OUTPUT}" "${INPUT}"',
    #     base_id="ido6.0",
    # )
-   print("Not implemented yet")
 else:
     IDO60_IRIX = IDOCompiler(
         id="ido6.0_irix",
@@ -402,7 +401,6 @@ if os.environ.get("RUNNING_IN_DOCKER") == "1":
    #     cc='${QEMU_IRIX} -L /compilers/ido6.0 "${COMPILER_DIR}/usr/bin/cc" -c -Xcpluscomm -G0 -non_shared -Wab,-r4300_mul -woff 649,838,712 -32 ${COMPILER_FLAGS} -o "${OUTPUT}" "${INPUT}"',
    #     base_id="ido6.0",
    # )
-print("Not implemented yet")
 else:
     IDO60 = IDOCompiler(
         id="ido6.0",
