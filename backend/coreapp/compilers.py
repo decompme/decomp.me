31669bc94deb86aa906e9b1ff934e174ae6cf8a1
LLONSIT <lonsitsyt456@gmail.com>
2023-02-09 20:58:37 -0500

    update

--- backend/coreapp/compilers.py
+++ backend/coreapp/compilers.py
@@ -362,7 +362,7 @@ else:
     IDO60_IRIX = IDOCompiler(
         id="ido6.0_irix",
         platform=IRIX,
-        cc='${QEMU_IRIX} -L "${COMPILER_DIR}" "${COMPILER_DIR}/usr/bin/cc" -c -Xcpluscomm -G0 -non_shared -Wab,-r4300_mul -woff 649,838,712 -32 ${COMPILER_FLAGS} -o "${OUTPUT}" "${INPUT}"',
+         cc='IDO_CC="${COMPILER_DIR}/cc" "${COMPILER_DIR}/cc" -c -Xcpluscomm -G0 -non_shared -woff 649,838,712 -32 ${COMPILER_FLAGS} -o "${OUTPUT}" "${INPUT}"',
         base_id="ido6.0",
     )
 
@@ -405,7 +405,7 @@ else:
     IDO60 = IDOCompiler(
         id="ido6.0",
         platform=N64,
-        cc='${QEMU_IRIX} -L "${COMPILER_DIR}" "${COMPILER_DIR}/usr/bin/cc" -c -Xcpluscomm -G0 -non_shared -Wab,-r4300_mul -woff 649,838,712 -32 ${COMPILER_FLAGS} -o "${OUTPUT}" "${INPUT}"',
+        cc='IDO_CC="${COMPILER_DIR}/cc" "${COMPILER_DIR}/cc" -c -Xcpluscomm -G0 -non_shared -woff 649,838,712 -32 ${COMPILER_FLAGS} -o "${OUTPUT}" "${INPUT}"',
         base_id="ido6.0",
     )
 
