e03b3574054408fa7b256388bf61b1e04a76b519
Alex Bates <alex@nanaian.town>
2023-02-23 00:36:14 +0000

    dynamically render scratch pages (#688)

--- frontend/src/app/scratch/[slug]/page.tsx
+++ frontend/src/app/scratch/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import getScratchDetails from "./getScratchDetails"
 import ScratchEditor from "./ScratchEditor"
 
+// Always server side render, avoiding caching scratch details
+export const dynamic = "force-dynamic"
+
 export default async function Page({ params }: { params: { slug: string }}) {
     const { scratch, parentScratch, compilation } = await getScratchDetails(params.slug)
 
