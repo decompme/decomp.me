d61a6ea952e3709d9de2237e94a14c32457f3dfe
sozud <122322823+sozud@users.noreply.github.com>
2023-04-09 00:27:01 -0400

    Initial Saturn support (#728)
    * Saturn support
    
    * linter issues
    
    * Fix black formatting issues
    
    * Try fixing ci
    
    * Try 3.10.0
    
    * Try 3.9
    
    * Update asm-differ hash
    
    ---------
    
    Co-authored-by: sozud <sozud@users.noreply.github.com>

--- frontend/src/components/PlatformSelect/PlatformIcon.tsx
+++ frontend/src/components/PlatformSelect/PlatformIcon.tsx
@@ -8,6 +8,7 @@ import LogoN64 from "./n64.svg"
 import LogoNDS from "./nds.svg"
 import LogoPS1 from "./ps1.svg"
 import LogoPS2 from "./ps2.svg"
+import LogoSaturn from "./saturn.svg"
 import LogoSwitch from "./switch.svg"
 import UnknownIcon from "./unknown.svg"
 
@@ -24,6 +25,7 @@ const ICONS = {
     "ps2": LogoPS2,
     "n3ds": LogoN3DS,
     "switch": LogoSwitch,
+    "saturn": LogoSaturn,
 }
 
 export const PLATFORMS = Object.keys(ICONS)
