928af2363dc1c3697cca95ad53747f27b39ef1de
1superchip <1superchip@gmail.com>
2023-04-13 08:15:01 -0500

    Update Ty the Tasmanian Tiger PS2 Preset

--- backend/coreapp/compilers.py
+++ backend/coreapp/compilers.py
@@ -1466,7 +1466,7 @@ _all_presets = [
     Preset(
         "Ty the Tasmanian Tiger (July 1st)",
         EE_GCC29_991111,
-        "-x c++ -O2 -fno-exceptions -gstabs",
+        "-x c++ -O2 -fno-exceptions -gstabs -ffast-math -finline-functions",
     ),
     Preset(
         "Sunny Garcia Surfing",
