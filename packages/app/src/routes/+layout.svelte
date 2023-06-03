0376f8bb159bdf5cbebf3962430535bd368bdbee
Alex Bates <alex@nanaian.town>
2023-06-03 18:27:24 +0100

    add tailwindcss and scss

---
+++ packages/app/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.scss";
+    import "../app.scss";
+</script>
+
+<div class="flex flex-col bg-gray-1 font-sans text-gray-12 subpixel-antialiased">
+    <slot></slot>
+</div>
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,6 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+    plugins: [sveltekit()],
+
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@use "src/variables.scss" as *;',
+            },
+        },
+    },
+})
