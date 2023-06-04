3cae26ecded3e760d786f6149ae699fcaf2f1797
Alex Bates <alex@nanaian.town>
2023-06-04 13:04:32 +0100

    improve eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,20 +1,26 @@
 // TODO: port frontend/.eslintrc.json
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'turbo', 'prettier'],
-    plugins: ['svelte3', '@typescript-eslint'],
-    ignorePatterns: ['*.cjs'],
-    overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "turbo",
+        "prettier",
+        "plugin:tailwindcss/recommended",
+    ],
+    plugins: ["svelte3", "@typescript-eslint", "tailwindcss"],
+    ignorePatterns: ["*.cjs"],
+    overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
     settings: {
-      'svelte3/typescript': () => require('typescript')
+        "svelte3/typescript": () => require("typescript"),
     },
     parserOptions: {
-      sourceType: 'module',
-      ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaVersion: 2020,
     },
     env: {
-      browser: true,
-      es2017: true,
-      node: true
+        browser: true,
+        es2017: true,
+        node: true,
     },
-};
+}
