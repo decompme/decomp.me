933bd31299e32adfe377244cca49fb37e80dd967
Mark Street <streetster@gmail.com>
2023-09-15 17:47:41 +0100

    Assume msdos assembler lives on PATH

--- backend/coreapp/platforms.py
+++ backend/coreapp/platforms.py
@@ -45,9 +45,9 @@ MSDOS = Platform(
     name="Microsoft DOS",
     description="x86",
     arch="i686",
-    assemble_cmd='${COMPILER_BASE_PATH}/msdos/i386_tools/jwasm -c -Fo"$OUTPUT" "$INPUT"',
-    objdump_cmd="${COMPILER_BASE_PATH}/msdos/i386_tools/omf-objdump",
-    nm_cmd="${COMPILER_BASE_PATH}/msdos/i386_tools/omf-nm",
+    assemble_cmd='jwasm -c -Fo"$OUTPUT" "$INPUT"',
+    objdump_cmd="omf-objdump",
+    nm_cmd="omf-nm",
     asm_prelude="""
         .386P
         .model FLAT
