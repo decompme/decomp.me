7ede634d97363ad504bc945303fbd013c048b5e4
Mark Street <streetster@gmail.com>
2023-09-16 11:08:25 +0100

    Add --platforms and --compilers args to download.py

--- backend/compilers/download.py
+++ backend/compilers/download.py
@@ -18,14 +18,9 @@ import podman
 
 logger = logging.getLogger(__name__)
 
-COMPILERS_DIR: Path = Path(os.path.dirname(os.path.realpath(__file__)))
-DOWNLOAD_CACHE = COMPILERS_DIR / "download_cache"
-DOWNLOAD_CACHE.mkdir(exist_ok=True)
 
-# TODO: can we pull this out into json/yaml and load based on HOST_ARCH?
-HOST_ARCH = platform.system().lower()
-
-if HOST_ARCH == "darwin":
+# TODO: can we pull this out into json/yaml and load based on host arch instead?
+if platform.system().lower() == "darwin":
     COMPILERS = {
         "n64": [
             "ido5.3",
@@ -197,6 +192,8 @@ class ContainerManager:
     def create_container(self, docker_image, commands=None):
         if commands is None:
             commands = [""]
+        if not isinstance(commands, list):
+            commands = [commands]
         return self.client.containers.create(docker_image, command=commands)
 
 
@@ -262,37 +259,42 @@ class DockerManager(ContainerManager):
 def get_compiler(
     platform_id,
     compiler_id,
-    host_arch="linux",
-    podman=False,
+    compilers_dir="/tmp",
     force=False,
-    github_repo="mkst/compilers",
+    podman=False,
+    docker_registry="ghcr.io/mkst/compilers",
 ):
-    # TODO: seems to be issues trying to share a single instance?
-    client_manager = PodmanManager() if podman else DockerManager()
-
     logger.info("Processing %s (%s)", compiler_id, platform_id)
 
-    compiler_dir = COMPILERS_DIR / platform_id / compiler_id
-    image_digest = compiler_dir / ".image_digest"
+    # fast-fail if we cannot create the download_cache
+    download_cache = compilers_dir / "download_cache"
+    download_cache.mkdir(parents=True, exist_ok=True)
+
+    # TODO: podman seems to have issues sharing a single instance
+    client_manager = PodmanManager() if podman else DockerManager()
 
     clean_compiler_id = compiler_id.lower().replace("+", "plus")
-
-    # assume arch-less image to begin with
-    docker_image = f"ghcr.io/{github_repo}/{platform_id}/{clean_compiler_id}:latest"
+    docker_image = f"{docker_registry}/{platform_id}/{clean_compiler_id}:latest"
 
     logger.debug(f"Checking for %s in registry", docker_image)
     remote_image_digest = client_manager.get_remote_image_digest(docker_image)
     if remote_image_digest is None:
+        host_arch = platform.system().lower()
         logger.debug(
             f"%s not found in registry, checking for '%s' specific version",
             docker_image,
             host_arch,
         )
-        docker_image = f"ghcr.io/{github_repo}/{platform_id}/{clean_compiler_id}/{host_arch}:latest"
+        docker_image = (
+            f"{docker_registry}/{platform_id}/{clean_compiler_id}/{host_arch}:latest"
+        )
         remote_image_digest = client_manager.get_remote_image_digest(docker_image)
         if remote_image_digest is None:
             logger.error(f"%s not found in registry!", docker_image)
             return
+
+    compiler_dir = compilers_dir / platform_id / compiler_id
+    image_digest = compiler_dir / ".image_digest"
 
     if not compiler_dir.exists() or force is True:
         # we need to extract something, check if we need to pull the image
@@ -301,13 +303,16 @@ def get_compiler(
             client_manager.pull(docker_image)
         else:
             logger.info(
-                f"%s is present and at latest version, continuing!", compiler_id
+                f"%s (%s) image is present and at latest version, continuing!",
+                compiler_id,
+                platform_id,
             )
-
     else:
         # compiler_dir exists, is it up to date with remote?
         if image_digest.exists() and image_digest.read_text() == remote_image_digest:
-            logger.info(f"%s is present and at latest version, skipping!", compiler_id)
+            logger.info(
+                f"%s image is present and at latest version, skipping!", compiler_id
+            )
             return
         # image_digest missing or out of date, so pull
         logger.info("%s has newer image available; pulling ...", docker_image)
@@ -324,40 +329,45 @@ def get_compiler(
         stream, _ = container.get_archive(compiler_container_path)
     except Exception as err:
         logger.error(
-            "Unable to pull %s from container: %s", compiler_container_path, err
+            "Unable to pull '%s' from container: %s", compiler_container_path, err
         )
         container.remove()
         return
 
     try:
         with tempfile.TemporaryDirectory() as temp_dir:
-            tar_file = Path(temp_dir) / f"{compiler_id}.tar"
+            tar_file = Path(temp_dir) / f"{platform_id}_{compiler_id}.tar"
             logger.debug("Writing to %s...", tar_file)
             with tar_file.open("wb") as f:
                 for chunk in stream:
                     f.write(chunk)
 
-            logger.debug(f"Extracting %s to %s", tar_file, DOWNLOAD_CACHE)
-            shutil.unpack_archive(tar_file, DOWNLOAD_CACHE)
+            logger.debug(f"Extracting %s to %s", tar_file, download_cache)
+            shutil.unpack_archive(tar_file, download_cache)
 
             if compiler_dir.exists():
+                logger.debug("Removing existing compiler dir: %s", compiler_dir)
                 shutil.rmtree(compiler_dir)
 
             logger.debug(
-                f"shutil.move %s -> %s", DOWNLOAD_CACHE / compiler_id, compiler_dir
+                f"shutil.move %s -> %s", download_cache / compiler_id, compiler_dir
             )
-            shutil.move(str(DOWNLOAD_CACHE / compiler_id), str(compiler_dir))
+            shutil.move(str(download_cache / compiler_id), str(compiler_dir))
 
             image_digest.write_text(remote_image_digest)
 
             logger.info(
-                "%s was successfully downloaded into %s", compiler_id, compiler_dir
+                "%s (%s) was successfully downloaded into %s",
+                compiler_id,
+                platform_id,
+                compiler_dir,
             )
     except Exception as err:
         logger.error(err)
+        container.remove()
+        return False
 
     container.remove()
-
     return True
 
 
@@ -368,34 +378,60 @@ def main():
     )
     parser.add_argument(
         "--force",
-        help="(re)download compiler when latest image already present",
+        help="Force (re)downloading of compilers",
         action="store_true",
+    )
+    parser.add_argument(
+        "--compilers-dir",
+        type=str,
+        default=None,
+        help="Directory where compilers will be stored",
+    )
+    parser.add_argument(
+        "--platforms", type=str, nargs="+", help="Only run for these platforms"
+    )
+    parser.add_argument(
+        "--compilers", type=str, nargs="+", help="Only run for these compilers"
+    )
+    parser.add_argument(
+        "--docker-registry",
+        type=str,
+        default="ghcr.io/mkst/compilers",
+        help="Docker registry where the packages live",
     )
     parser.add_argument(
         "--threads", type=int, default=4, help="Number of download threads to use"
     )
-    parser.add_argument(
-        "--github-repo",
-        type=str,
-        default="mkst/compilers",
-        help="GitHub repo where the packages live",
-    )
+    parser.add_argument("--verbose", action="store_true", help="Enable DEBUG log level")
     args = parser.parse_args()
+
+    if args.verbose:
+        logger.setLevel("DEBUG")
+
+    if args.compilers_dir:
+        compilers_dir: Path = Path(args.compilers_dir)
+    else:
+        compilers_dir: Path = Path(os.path.dirname(os.path.realpath(__file__)))
 
     to_download = []
     for platform_id, compilers in COMPILERS.items():
-        # platforms are considered enabled unless explicitly disabled
-        platform_enabled = (
-            os.environ.get(f"ENABLE_{platform_id.upper()}_SUPPORT", "YES").upper()
-            != "NO"
-        )
-        if platform_enabled:
-            to_download += [(platform_id, compiler) for compiler in compilers]
+        if args.platforms is not None:
+            platform_enabled = platform_id in args.platforms
+        else:
+            # platforms are considered enabled unless explicitly disabled
+            platform_enabled = (
+                os.environ.get(f"ENABLE_{platform_id.upper()}_SUPPORT", "YES").upper()
+                != "NO"
+            )
+        if args.compilers is not None:
+            compilers = filter(lambda x: x in args.compilers, compilers)
+
+        to_download += [(platform_id, compiler) for compiler in compilers]
 
     if len(to_download) == 0:
         logger.warning(
-            "No platforms are configured to be downloaded for host architecture (%s)",
-            HOST_ARCH,
+            "No platforms/compilers configured or enabled for host architecture (%s)",
+            platform.system().lower(),
         )
         return
 
@@ -404,10 +440,10 @@ def main():
         results = pool.starmap(
             functools.partial(
                 get_compiler,
-                host_arch=HOST_ARCH,
+                compilers_dir=compilers_dir,
                 podman=args.podman,
                 force=args.force,
-                github_repo=args.github_repo,
+                docker_registry=args.docker_registry,
             ),
             to_download,
         )
@@ -426,10 +462,7 @@ if __name__ == "__main__":
     logging.basicConfig(
         handlers=[logging.StreamHandler()],
         level=logging.INFO,
-        format=(
-            "%(asctime)s.%(msecs)03d %(levelname)s %(filename)s "
-            + "%(funcName)s %(message)s"
-        ),
+        format=("%(asctime)s.%(msecs)03d %(levelname)s %(funcName)s %(message)s"),
         datefmt="%Y-%m-%d %H:%M:%S",
     )
     main()
