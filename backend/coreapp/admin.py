5081362cc8a796d7992f7dd93a1f92fd3b33da46
Ethan Roseman <ethteck@gmail.com>
2023-10-24 11:37:33 +0900

    Preset in admin panel & rename migration

--- backend/coreapp/admin.py
+++ backend/coreapp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models.course import Course, CourseChapter, CourseScenario
 from .models.github import GitHubUser
+from .models.preset import Preset
 from .models.profile import Profile
 from .models.project import Project, ProjectFunction, ProjectMember
 from .models.scratch import Asm, Assembly, CompilerConfig, Scratch
@@ -12,6 +13,7 @@ admin.site.register(Asm)
 admin.site.register(Assembly)
 admin.site.register(Scratch)
 admin.site.register(CompilerConfig)
+admin.site.register(Preset)
 admin.site.register(Project)
 admin.site.register(ProjectFunction)
 admin.site.register(ProjectMember)
