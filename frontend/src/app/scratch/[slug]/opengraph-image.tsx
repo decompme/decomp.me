28728fdcaaefcb54cabb747f8531564350b92d03
Mark Street <streetster@gmail.com>
2024-03-03 20:08:24 +0000

    tweaks

--- frontend/src/app/scratch/[slug]/opengraph-image.tsx
+++ frontend/src/app/scratch/[slug]/opengraph-image.tsx
@@ -2,6 +2,7 @@ import { ImageResponse } from "next/server"
 
 import { PlatformIcon } from "@/components/PlatformSelect/PlatformIcon"
 import { getScoreText } from "@/components/ScoreBadge"
+import { get } from "@/lib/api/request"
 
 import getScratchDetails from "./getScratchDetails"
 
@@ -14,52 +15,40 @@ export default async function ScratchOG({ params }: { params: { slug: string}}) 
     const score = getScoreText(compilation?.diff_output?.current_score ?? -1, compilation?.diff_output?.max_score ?? -1, scratch.match_override)
     const fork = parentScratch ? `${parentScratch.owner?.username ?? "?"}/${parentScratch.name}` : ""
 
-    // this is really rubbish, need to either align the text at the botom,
-    // or in the middle without resorting to padding...
-    const getTextSize = text => {
-        if (!text)
-            return ""
-
-        if (text.length < 10) {
-            return "text-7xl"
-        } else if (text.length < 20) {
-            return "text-6xl pt-4"
-        } else if (text.length < 40) {
-            return "text-5xl pt-8"
-        } else {
-            return "text-4xl pt-9"
-        }
-    }
-
-    // FIXME: fetch preset from API based on scratch.preset
-    const presetName = "Dummy Preset"
+    const compilers = await get("/compiler")
+    const preset = compilers["platforms"][scratch.platform].presets.find(p => p.id === scratch.preset)
 
     return new ImageResponse(
         <div
-            tw="flex flex-col w-full h-full bg-neutral-800 text-slate-200 text-7xl leading-tight"
+            tw="flex flex-col justify-between w-full h-full bg-zinc-800 text-slate-200 text-5xl leading-normal"
+            className="dark"
         >
             <div
-                tw="flex ml-10 mt-10 mb-10"
+                tw="flex ml-10 mt-10 mr-10"
             >
                 <PlatformIcon
                     platform={scratch.platform}
                     size={96}
                 />
-                <div tw={`text-slate-50 ml-10 ${getTextSize(scratch.name)}`}>{scratch.name}</div>
+                <div tw={"text-slate-50 ml-10"}>{preset?.name || "Custom Preset"}</div>
+            </div>
+
+            <div tw="h-4 bg-fuchsia-600">
+
             </div>
 
             <div
-                tw="flex ml-10 mr-10 justify-between "
+                tw="flex ml-10 mr-10 justify-between"
             >
-                <div tw="">Preset:</div>
-                <div tw={`text-slate-50 ml-10 ${getTextSize(presetName)}`}>{presetName}</div>
+                <div tw="">Function:</div>
+                <div tw="text-slate-50 ml-10">{scratch.name}</div>
             </div>
 
             <div
-                tw="flex ml-10 mr-10 justify-between "
+                tw="flex ml-10 mr-10 justify-between"
             >
                 <div tw="">Owner:</div>
-                <div tw={`text-slate-50 ml-10 ${getTextSize(scratch.owner.username)}`}>{scratch.owner.username}</div>
+                <div tw="text-slate-50 ml-10">{scratch.owner.username}</div>
             </div>
 
             <div
@@ -71,16 +60,19 @@ export default async function ScratchOG({ params }: { params: { slug: string}}) 
 
             {fork && (
                 <div
-                    tw="flex ml-10 mr-10 justify-between"
+                    tw="flex ml-10 mr-10 mb-10 justify-between"
                 >
                     <div tw="">Parent:</div>
-                    <div tw={`text-slate-50 ml-10 ${getTextSize(fork)}`}>{fork}</div>
+                    <div tw="flex flex-col justify-center">
+                        <div tw="flex "></div>
+                        <div tw="flex text-slate-50 ml-10 text-3xl">{fork}</div>
+                    </div>
                 </div>
             )}
         </div>,
         {
             width: 1200,
-            height: fork ? 630 : 510,
+            height: 630,
         },
     )
 }
