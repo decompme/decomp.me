fd5891c3552d90522161b55c16436b4b585ad8c1
Mark Street <streetster@gmail.com>
2024-03-08 19:26:41 +0000

    tweaks

--- frontend/src/app/scratch/[slug]/opengraph-image.tsx
+++ frontend/src/app/scratch/[slug]/opengraph-image.tsx
@@ -20,21 +20,23 @@ export default async function ScratchOG({ params }: { params: { slug: string }})
     const compilers = await get("/compiler")
     const preset = compilers["platforms"][scratch.platform].presets.find(p => p.id === scratch.preset)
 
-    const scratchNameSize = scratch.name.length > 20 ? "text-5xl" : "text-6xl"
+    // This should cover most scratch names
+    const scratchNameSize =
+        scratch.name.length > 45 ? "text-3xl" :
+            scratch.name.length > 35 ? "text-4xl" :
+                scratch.name.length > 25 ? "text-5xl" : "text-6xl"
 
     const score = compilation?.diff_output?.current_score ?? -1
     const maxScore = compilation?.diff_output?.max_score ?? -1
 
-    scratch.match_override = true
-
     return new ImageResponse(
         <div
-            tw="flex flex-col justify-between w-full h-full bg-zinc-800 text-slate-50"
+            tw="flex flex-col justify-between w-full h-full bg-zinc-800 text-slate-50 text-5xl"
             className="dark"
         >
-            <div tw="flex ml-20 mr-20 mt-5 flex-row justify-between">
+            <div tw="flex ml-15 mr-15 mt-5 flex-row justify-between">
                 <div tw="flex flex-col justify-center">
-                    <div tw="flex text-5xl text-slate-300">{scratch.owner.username}</div>
+                    <div tw="flex text-slate-300">{scratch.owner.username}</div>
                     <div tw={`flex ${scratchNameSize}`}>{scratch.name}</div>
                 </div>
                 <div tw="flex bg-zinc-700 rounded-2xl">
@@ -47,9 +49,9 @@ export default async function ScratchOG({ params }: { params: { slug: string }})
                 </div>
             </div>
 
-            <div tw="flex ml-20 mr-20 text-4xl text-slate-300">{preset?.name || "Custom Preset"}</div>
+            <div tw="flex ml-15 mr-15 text-slate-300">{preset?.name || "Custom Preset"}</div>
 
-            <div tw="flex ml-20 mr-20 justify-between">
+            <div tw="flex ml-15 mr-15 justify-between">
                 <div tw="flex">
 
                     {score === -1
@@ -70,9 +72,9 @@ export default async function ScratchOG({ params }: { params: { slug: string }})
                                     <CheckCircleFillIcon width={48} height={48}/>
                                 </div>
                                 <div tw="flex flex-col ml-4 justify-around">
-                                    <div tw="flex text-5xl">Matched</div>
+                                    <div tw="flex">Matched</div>
                                     {scratch.match_override &&
-                                        <div tw="flex text-4xl text-slate-300">(Override)</div>
+                                        <div tw="flex text-4xl text-slate-300 align-center">(Override)</div>
                                     }
                                 </div>
                             </div>
@@ -82,20 +84,20 @@ export default async function ScratchOG({ params }: { params: { slug: string }})
                                     <TrophyIcon width={40} height={40} />
                                 </div>
                                 <div tw="flex flex-col justify-around ml-4">
-                                    <div tw="flex text-4xl text-slate-300">{score}</div>
-                                    <div tw="flex text-5xl">{percentToString(calculateScorePercent(score, maxScore))}</div>
+                                    <div tw="flex">
+                                        {score} ({percentToString(calculateScorePercent(score, maxScore))})
+                                    </div>
                                 </div>
                             </div>
                     }
 
                     {parentScratch &&
-                    <div tw="flex flex-row ml-5">
+                    <div tw="flex flex-row ml-4 ">
                         <div tw="flex flex-col justify-around">
                             <RepoForkedIcon width={40} height={40} />
                         </div>
-                        <div tw="flex flex-col ml-4 mt-1">
-                            <div tw="flex text-4xl text-slate-300">{parentScratch.owner?.username ?? "?"}</div>
-                            <div tw="flex text-5xl">{parentScratch.name}</div>
+                        <div tw="flex flex-col justify-around ml-4">
+                            {parentScratch.owner?.username ?? "Anonymous User"}
                         </div>
                     </div>
                     }
