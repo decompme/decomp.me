b901c54c8ea431f53f8f71c791aafdcb600d30f6
Ethan <ethteck@gmail.com>
2024-03-10 21:59:14 +0900

    more changes

--- frontend/src/misc_types.d.ts
+++ frontend/src/misc_types.d.ts
@@ -26,3 +26,11 @@ declare module "is-dark-color" {
 
     export = createPersistedState;
   }
+
+  declare module "*.svg" {
+    import React from "react"
+
+    const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+
+    export default content
+  }
