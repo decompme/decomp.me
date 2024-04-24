d09c8c11052a72aeda018c3a356bbc236d6b3c31
Mark Street <22226349+mkst@users.noreply.github.com>
2024-04-24 20:49:45 +0100

    Tweak opengraph rendinger speed (#1212)
    * Tweak opengraph rendinger speed
    
    * tweak++

--- frontend/src/app/opengraph-image.tsx
+++ frontend/src/app/opengraph-image.tsx
@@ -20,37 +20,39 @@ export default async function HomeOG() {
     const OpenSansBold = fetch(new URL("/public/fonts/OpenSans-Bold.ttf", import.meta.url)).then(res =>
         res.arrayBuffer()
     )
+
     const statsRes = await fetch("http://decomp.me/api/stats")
     const stats = await statsRes.json()
-    const icon_size = 150
+    const iconSize = 160
+    const iconCount = 5
     const textScale = 4.15
     const textSize = {
         title: textScale,
         description: 0.6 * textScale,
-        stats: 0.3 * textScale,
+        stats: 0.45 * textScale,
     }
     return new ImageResponse(
         (
             <div
                 tw="flex flex-col w-full h-full bg-zinc-900 text-slate-50 items-center justify-center">
-                <div tw="absolute flex flex-row items-center h-full gap-2 opacity-40">
+                <div tw="absolute flex flex-row items-center h-full opacity-25">
                     {PLATFORMS.map(platform => ({ platform, sort: Math.random() }))
                         .sort((a, b) => a.sort - b.sort)
+                        .slice(0, iconCount)
                         .map(({ platform }) => {
                             const Icon = platformIcon(platform)
-                            return (<Icon key={platform} width={icon_size} height={icon_size} tw="ml-16 shadow-2xl" style={{ filter: "blur(2px)" }} />)
+                            return (<Icon key={platform} width={iconSize} height={iconSize} tw="m-10" />)
                         })}
                 </div>
                 <div tw="w-full h-1/10" />
-                <div tw={`flex w-full justify-center text-[${textSize.title}rem] text-gray-12`} style={{ fontFamily: "OpenSans-ExtraBold" }}>decomp.me</div>
-                <a tw={`flex flex-wrap justify-center text-[${textSize.description}rem] text-gray-12 text-center`} style={{ fontFamily: "OpenSans-Bold" }}>Collaboratively decompile code in your</a>
-                <a tw={`flex flex-wrap justify-center text-[${textSize.description}rem] text-gray-12`} style={{ fontFamily: "OpenSans-Bold" }}>browser</a>
+                <div tw={`flex w-full justify-center text-[${textSize.title}rem]`} style={{ fontFamily: "OpenSans-ExtraBold" }}>decomp.me</div>
+                <span tw={`flex flex-wrap justify-center text-[${textSize.description}rem] text-center w-3/4`} style={{ fontFamily: "OpenSans-Bold" }}>Collaboratively decompile code in your browser</span>
                 <div tw="w-full h-1/8" />
                 <div tw={`flex justify-between w-full text-[${textSize.stats}rem]`} style={{ fontFamily: "OpenSans-SemiBold" }}>
-                    <a tw="ml-10">{stats.scratch_count.toLocaleString()} scratches created</a>
-                    <a>{stats.profile_count.toLocaleString()} unique visitors</a>
-                    <a>{stats.github_user_count.toLocaleString()} users signed up</a>
-                    <a tw="mr-10">{stats.asm_count.toLocaleString()} asm globs submitted</a>
+                    <a tw="ml-10">{stats.scratch_count.toLocaleString()} scratches</a>
+                    <a>{stats.profile_count.toLocaleString()} visitors</a>
+                    <a>{stats.github_user_count.toLocaleString()} users</a>
+                    <a tw="mr-10">{stats.asm_count.toLocaleString()} asm globs</a>
                 </div>
             </div>
         ),
