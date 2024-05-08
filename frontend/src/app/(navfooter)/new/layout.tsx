a65f46e0d8c0fe6ad4ee3171ee75cd90259f1fcd
Conor Golden <conorgolden1@hotmail.com>
2024-05-08 09:38:58 -0400

    Restrained max width on new scratch

--- frontend/src/app/(navfooter)/new/layout.tsx
+++ frontend/src/app/(navfooter)/new/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react"
 
 export default function Layout({ children }: { children: ReactNode }) {
-    return <div className="mx-auto max-w-screen-lg p-4 pb-2 text-justify text-base leading-normal">
+    return <div className="mx-auto max-w-[46.5rem] p-4 pb-2 text-justify text-base leading-normal">
         {children}
     </div>
 }
