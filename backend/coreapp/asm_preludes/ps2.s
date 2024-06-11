1d98f54ab46650a409fbea40e9a18d85702ae964
1superchip <1superchip@gmail.com>
2024-06-11 12:31:56 -0500

    Update ps2 asm prelude
    Updates existing macros and adds more macros to support declaring visibility for symbols

--- backend/coreapp/asm_preludes/ps2.s
+++ backend/coreapp/asm_preludes/ps2.s
@@ -1,20 +1,66 @@
-.macro glabel label
-    .global \label
-    .type \label, @function
-    \label:
+.macro glabel name, visibility=global
+.\visibility "\name"
+.type "\name", @function
+"\name":
 .endm
 
-.macro dlabel label
-    .global \label
-    \label:
+.macro dlabel name, visibility=global
+.\visibility "\name"
+.type "\name", @object
+"\name":
 .endm
 
-.macro jlabel label
-    \label:
+.macro jlabel name, visibility=local
+.\visibility "\name"
+"\name":
 .endm
 
 .macro .late_rodata
     .section .rodata
+.endm
+
+# Defines a sized symbol with function type.
+# Usage:
+# .fn my_function, local
+# /* asm here */
+# .endfn my_function
+.macro .fn name, visibility=global
+.\visibility "\name"
+.type "\name", @function
+"\name":
+.endm
+
+.macro .endfn name
+.size "\name", . - "\name"
+.endm
+
+# Defines a sized symbol with object type.
+# Usage:
+# .obj my_object, local
+# /* data here */
+# .endobj my_object
+.macro .obj name, visibility=global
+.\visibility "\name"
+.type "\name", @object
+"\name":
+.endm
+
+.macro .endobj name
+.size "\name", . - "\name"
+.endm
+
+# Defines a sized symbol without a type.
+# Usage:
+# .sym my_sym, local
+# /* anything here */
+# .endsym my_sym
+.macro .sym name, visibility=global
+.\visibility "\name"
+"\name":
+.endm
+
+.macro .endsym name
+.size "\name", . - "\name"
 .endm
 
 .set noat
