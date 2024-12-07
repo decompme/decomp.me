3506080216678007d18f5b0a63f0fd138ce995ff
Ethan <ethteck@gmail.com>
2024-12-08 03:41:47 +0900

    Select

--- frontend/src/components/Select2.tsx
+++ frontend/src/components/Select2.tsx
@@ -1,8 +1,5 @@
 import { useEffect } from "react";
-
 import { ChevronDownIcon } from "@primer/octicons-react";
-
-import styles from "./Select.module.scss";
 
 export type Props = {
     options: { [key: string]: string };
@@ -17,12 +14,15 @@ export default function Select({ options, value, onChange, className }: Props) {
     }, [value, options, onChange]);
 
     return (
-        <div className={`${styles.group} ${className}`}>
+        <div
+            className={`relative inline-flex select-none rounded border border-[var(--g400)] bg-[var(--g200)] px-[10px] py-2 text-[0.8rem] text-[var(--g1600)] ${className}`}
+        >
             <select
                 value={value}
                 onChange={(event) => {
                     onChange(event.target.value);
                 }}
+                className="!outline-0 flex-1 appearance-none border-0 bg-transparent pr-8 outline-none [&_option]:bg-[var(--g200)] [&_option]:text-[var(--g1600)]"
             >
                 {Object.entries(options).map(([key, name]) => (
                     <option key={key} value={key}>
@@ -31,7 +31,7 @@ export default function Select({ options, value, onChange, className }: Props) {
                 ))}
             </select>
 
-            <div className={styles.icon}>
+            <div className="-translate-y-1/2 pointer-events-none absolute top-1/2 right-[0.6em]">
                 <ChevronDownIcon size={16} />
             </div>
         </div>
