577a44864e0dbed678ac95e18ec8dc7fd3af4ac9
Mark Street <streetster@gmail.com>
2025-02-12 16:06:52 +0000

    mypy

--- backend/coreapp/tests/common.py
+++ backend/coreapp/tests/common.py
@@ -18,7 +18,7 @@ def requiresCompiler(*compilers: Compiler) -> Callable[..., Any]:
 
 class BaseTestCase(APITestCase):
 
-    def setUp(self):
+    def setUp(self) -> None:
         super().setUp()
         self.client.credentials(HTTP_USER_AGENT="Firefrogz 1.0")
 
