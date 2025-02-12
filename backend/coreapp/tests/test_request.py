13100aad5c055be62ece077289bfa5a0287fd43b
Mark Street <streetster@gmail.com>
2025-02-12 16:22:22 +0000

    fixed 1

--- backend/coreapp/tests/test_request.py
+++ backend/coreapp/tests/test_request.py
@@ -7,13 +7,13 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 
-class RequestTests(BaseTestCase):
+class RequestTests(APITestCase):
     def test_create_profile(self) -> None:
         """
         Ensure that we create a profile for a normal request
         """
 
-        response = self.client.get(reverse("compilers"))
+        response = self.client.get(reverse("compilers"), HTTP_USER_AGENT="browser")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         self.assertEqual(Profile.objects.count(), 1)
