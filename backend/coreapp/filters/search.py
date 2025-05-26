1ccdbc4be5dc8796e48faa9dad15dd7e5b09211a
Mark Street <streetster@gmail.com>
2025-05-26 10:18:40 +0100

    black

--- backend/coreapp/filters/search.py
+++ backend/coreapp/filters/search.py
@@ -1,6 +1,7 @@
 from rest_framework.filters import SearchFilter
 from rest_framework.request import Request
 
+
 class NonEmptySearchFilter(SearchFilter):
     # Skip empty values to avoid LIKE "%%" clauses
     def get_search_terms(self, request: Request) -> list[str]:
