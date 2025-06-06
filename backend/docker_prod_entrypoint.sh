87cff485f4cade39ef1391fcd5dc1dfe711e45ec
Mark Street <streetster@gmail.com>
2025-06-06 14:11:08 +0100

    🐻 hide all the dumbness

--- backend/docker_prod_entrypoint.sh
+++ backend/docker_prod_entrypoint.sh
@@ -15,4 +15,13 @@ done
 
 poetry run /backend/manage.py migrate
 
+if command -v regedit &> /dev/null; then
+  for reg in /backend/wine/*.reg; do
+    echo "Importing registry file $reg..."
+    regedit $reg
+  done
+else
+  echo "regedit command not found. Skipping registry import."
+fi
+
 poetry run gunicorn -w ${WORKERS} decompme.wsgi --bind ${BE_HOST}:${BE_PORT}
