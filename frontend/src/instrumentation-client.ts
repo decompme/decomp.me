515f9e97f44b05cd0fdd63d4477dab73fab01ed7
Mark Street <22226349+mkst@users.noreply.github.com>
2025-06-15 09:30:42 +0200

    Enable sentry.io in frontend (#1607)
    * Enable sentry.io in frontend
    
    * Skip housekeeping.py when running in CI
    
    * tweaks
    
    * remove .yarn/install-state.gz

---
+++ frontend/src/instrumentation-client.ts
@@ -0,0 +1,23 @@
+import { init, captureRouterTransitionStart } from "@sentry/nextjs";
+
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+const isSentryEnabled = !!SENTRY_DSN;
+
+if (isSentryEnabled) {
+    init({
+        dsn: SENTRY_DSN,
+
+        tracesSampleRate: Number.parseFloat(
+            process.env.NEXT_PUBLIC_SENTRY_CLIENT_SAMPLE_RATE ?? "0.1",
+        ),
+
+        debug: false,
+    });
+} else {
+    console.log("Sentry is disabled on the client.");
+}
+
+export const onRouterTransitionStart = isSentryEnabled
+    ? captureRouterTransitionStart
+    : () => {};
