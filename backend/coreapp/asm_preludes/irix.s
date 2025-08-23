18fb66821791abc688f545b5c604cee8ad5e8290
Anghelo Carvajal <angheloalf95@gmail.com>
2025-08-23 10:24:38 -0400

    Update labels used by splat

--- backend/coreapp/asm_preludes/irix.s
+++ backend/coreapp/asm_preludes/irix.s
@@ -1,17 +1,59 @@
-.macro glabel label
-    .global \label
+# A function symbol.
+.macro glabel label, visibility=global
+    .\visibility \label
     .type \label, @function
     \label:
+        .ent \label
 .endm
 
-.macro dlabel label
-    .global \label
+# The end of a function symbol.
+.macro endlabel label
+    .size \label, . - \label
+    .end \label
+.endm
+
+# An alternative entry to a function.
+.macro alabel label, visibility=global
+    .\visibility \label
+    .type \label, @function
+    \label:
+        .aent \label
+.endm
+
+# A label referenced by an error handler table.
+.macro ehlabel label, visibility=global
+    .\visibility \label
     \label:
 .endm
 
-.macro jlabel label
+
+# A data symbol.
+.macro dlabel label, visibility=global
+    .\visibility \label
+    .type \label, @object
     \label:
 .endm
+
+# End of a data symbol.
+.macro enddlabel label
+    .size \label, . - \label
+.endm
+
+
+# A label referenced by a jumptable.
+.macro jlabel label, visibility=global
+    \label:
+.endm
+
+
+# Label to signal the symbol haven't been matched yet.
+.macro nmlabel label, size=1
+    .global \label\().NON_MATCHING
+    .type \label\().NON_MATCHING, @object
+    .size \label\().NON_MATCHING, \size
+    \label\().NON_MATCHING:
+.endm
+
 
 .macro .late_rodata
     .section .rodata
--- backend/coreapp/asm_preludes/ps2.s
+++ backend/coreapp/asm_preludes/ps2.s
@@ -1,23 +1,60 @@
-.macro glabel name, visibility=global
-.\visibility "\name"
-.type "\name", @function
-"\name":
+# A function symbol.
+.macro glabel label, visibility=global
+    .\visibility "\label"
+    .type "\label", @function
+    "\label":
+        .ent "\label"
 .endm
 
-.macro dlabel name, visibility=global
-.\visibility "\name"
-.type "\name", @object
-"\name":
+# The end of a function symbol.
+.macro endlabel label
+    .size "\label", . - "\label"
+    .end "\label"
 .endm
 
-.macro jlabel name, visibility=local
-.\visibility "\name"
-"\name":
+# An alternative entry to a function.
+.macro alabel label, visibility=global
+    .\visibility "\label"
+    .type "\label", @function
+    "\label":
+        .aent "\label"
 .endm
 
-.macro .late_rodata
-    .section .rodata
+# A label referenced by an error handler table.
+.macro ehlabel label, visibility=global
+    .\visibility "\label"
+    "\label":
 .endm
+
+
+# A data symbol.
+.macro dlabel label, visibility=global
+    .\visibility \label
+    .type \label, @object
+    \label:
+.endm
+
+# End of a data symbol.
+.macro enddlabel label
+    .size "\label", . - "\label"
+.endm
+
+
+# A label referenced by a jumptable.
+.macro jlabel label, visibility=local
+    .\visibility "\label"
+    "\label":
+.endm
+
+
+# Label to signal the symbol haven't been matched yet.
+.macro nmlabel label, size=1
+    .global "\label\().NON_MATCHING"
+    .type "\label\().NON_MATCHING", @object
+    .size "\label\().NON_MATCHING", \size
+    "\label\().NON_MATCHING":
+.endm
+
 
 # Defines a sized symbol with function type.
 # Usage:
