aca8070f89393fca99a773b833bfe22a7c6fb588
Mark Street <22226349+mkst@users.noreply.github.com>
2025-09-08 18:56:37 +0100

    Fix settings page icon alignment (#1655)

--- frontend/src/components/GhostButton.tsx
+++ frontend/src/components/GhostButton.tsx
@@ -18,11 +18,14 @@ export default function GhostButton({
     className,
 }: Props) {
     const isClickable = !!(href || onClick);
-    const cn = clsx(className, {
-        "rounded bg-transparent px-2 py-1 text-sm whitespace-nowrap inline-block": true,
-        "transition-colors hover:bg-gray-3 cursor-pointer active:translate-y-px hover:text-gray-12":
-            isClickable,
-    });
+    const cn = clsx(
+        className,
+        "flex items-center gap-2 rounded bg-transparent px-2 py-1 text-sm whitespace-nowrap",
+        {
+            "transition-colors hover:bg-gray-3 cursor-pointer active:translate-y-px hover:text-gray-12":
+                isClickable,
+        },
+    );
 
     if (href) {
         return (
