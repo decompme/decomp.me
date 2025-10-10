fac5c62d4b2fa3a765e36a4e466ef7942a0ccbbb
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2025-10-10 18:56:47 +0200

    Fix bug in scratch language detection

--- backend/coreapp/serializers.py
+++ backend/coreapp/serializers.py
@@ -259,7 +259,7 @@ class ScratchSerializer(serializers.ModelSerializer[Scratch]):
             "platform",
         ]
 
-    def get_language(self, scratch: Scratch) -> Optional[str]:
+    def get_language(self, scratch: Scratch) -> str:
         """
         Strategy for extracting a scratch's language:
         - If the scratch's compiler has a LanguageFlagSet in its flags, attempt to match a language flag against that
@@ -267,24 +267,29 @@ class ScratchSerializer(serializers.ModelSerializer[Scratch]):
         """
         compiler = compilers.from_id(scratch.compiler)
         language_flag_set = next(
-            iter([i for i in compiler.flags if isinstance(i, LanguageFlagSet)]),
+            (i for i in compiler.flags if isinstance(i, LanguageFlagSet)),
             None,
         )
 
+        # We sort by match length to avoid having a partial match
         if language_flag_set:
             language = next(
                 iter(
-                    [
-                        language
-                        for (flag, language) in language_flag_set.flags.items()
-                        if flag in scratch.compiler_flags
-                    ]
+                    sorted(
+                        (
+                            (flag, language)
+                            for flag, language in language_flag_set.flags.items()
+                            if flag in scratch.compiler_flags
+                        ),
+                        key=lambda l: len(l[0]),
+                        reverse=True,
+                    )
                 ),
                 None,
             )
 
             if language:
-                return language.value
+                return language[1].value
 
         # If we're here, either the compiler doesn't have a LanguageFlagSet, or the scratch doesn't
         # have a flag within it.
