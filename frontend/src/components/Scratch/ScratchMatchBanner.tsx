348dc84f1456af5357c25d36bfbb7da013344a76
Mark Street <streetster@gmail.com>
2025-11-30 09:24:55 +0000

    Display banner when another scratch improves upon current scratch

--- frontend/src/components/Scratch/ScratchMatchBanner.tsx
+++ frontend/src/components/Scratch/ScratchMatchBanner.tsx
@@ -19,20 +19,26 @@ export default function ScratchMatchBanner({
         },
     );
 
-    // Consciously not including match_override here, since it's not really banner-worthy
-    const match = data?.find((s) => s.score === 0 && s.slug !== scratch.slug);
+    const match = data
+        ?.filter((s) => s.slug !== scratch.slug && s.score < scratch.score && s.score > -1)
+        .reduce((lowest, current) => {
+            return !lowest || current.score < lowest.score ? current : lowest;
+        }, null);
 
     if (error) throw error;
 
     if (scratch.score === 0 || !match) return null;
 
-    let message = "This function has been matched";
+    let message = `This function has been ${match.score === 0 ? "matched" : `improved (score: ${match.score.toLocaleString("en-US")})`}`;
     if (userIsYou(match.owner)) message += " by you, elsewhere";
     else if (match.owner) message += ` by ${match.owner.username}`;
 
     return (
         <DismissableBanner>
-            {message}. <Link href={scratchUrl(match)}>View match</Link>
+            {message}.{" "}
+            <Link href={scratchUrl(match)}>
+                View {match.score === 0 ? "match" : "improvement"}
+            </Link>
         </DismissableBanner>
     );
 }
