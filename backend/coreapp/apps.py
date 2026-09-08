9ecdd428774d43b0be9e92fb42d869fb4d0291c7
Ethan Roseman <ethteck@gmail.com>
2026-09-08 11:15:20 +0100

    remove dumb stuff

--- backend/coreapp/apps.py
+++ backend/coreapp/apps.py
@@ -1,9 +1,4 @@
-import logging
-
 from django.apps import AppConfig
-
-logger = logging.getLogger(__name__)
-
 
 class CoreappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
